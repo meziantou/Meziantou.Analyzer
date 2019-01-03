3928e25762220d36e4be93d895191f0ee83b890a
meziantou <meziantou@live.fr>
2019-01-03 14:25:44 -0500

    Rewrite using IOperation

---
+++ src/Meziantou.Analyzer/Internals/TypeSymbolExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Microsoft.CodeAnalysis;
+
+namespace Meziantou.Analyzer
+{
+    // http://source.roslyn.io/#Microsoft.CodeAnalysis.Workspaces/Shared/Extensions/ITypeSymbolExtensions.cs,190b4ed0932458fd,references
+    internal static class TypeSymbolExtensions
+    {
+        public static IList<INamedTypeSymbol> GetAllInterfacesIncludingThis(this ITypeSymbol type)
+        {
+            var allInterfaces = type.AllInterfaces;
+            if (type is INamedTypeSymbol namedType && namedType.TypeKind == TypeKind.Interface && !allInterfaces.Contains(namedType))
+            {
+                var result = new List<INamedTypeSymbol>(allInterfaces.Length + 1);
+                result.Add(namedType);
+                result.AddRange(allInterfaces);
+                return result;
+            }
+
+            return allInterfaces;
+        }
+
+        public static bool IsString(this ITypeSymbol symbol)
+        {
+            if (symbol == null)
+                return false;
+
+            return symbol.SpecialType == SpecialType.System_String;
+        }
+    }
+}
