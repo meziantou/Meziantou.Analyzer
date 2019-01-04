6897ffa5cca525782ecfcfb141c98771325f7313
meziantou <meziantou@live.fr>
2019-01-04 15:13:20 -0500

    MA0005 doesn't report diagnostic for `params` arguments

--- src/Meziantou.Analyzer/UsageRules/UseArrayEmptyAnalyzer.cs
+++ src/Meziantou.Analyzer/UsageRules/UseArrayEmptyAnalyzer.cs
@@ -45,17 +45,88 @@ namespace Meziantou.Analyzer.UsageRules
         private void AnalyzeArrayCreationOperation(OperationAnalysisContext context)
         {
             var operation = (IArrayCreationOperation)context.Operation;
-            if (operation.DimensionSizes.Length != 1)
-                return;
-
-            var dimensionSize = operation.DimensionSizes[0].ConstantValue;
-            if (dimensionSize.HasValue && (int)dimensionSize.Value == 0)
+            if (IsZeroLengthArrayCreation(operation))
             {
-                // Cannot use Array.Empty<T>() is an attribute
-                if (operation.Syntax.Ancestors().OfType<AttributeArgumentSyntax>().Any())
+                // Cannot use Array.Empty<T>() as an attribute parameter
+                if (IsInAttribute(operation))
+                    return;
+
+                if (IsCompilerGeneratedParamsArray(operation, context))
                     return;
 
                 context.ReportDiagnostic(Diagnostic.Create(s_rule, operation.Syntax.GetLocation()));
+            }
+        }
+
+        private static bool IsZeroLengthArrayCreation(IArrayCreationOperation operation)
+        {
+            if (operation.DimensionSizes.Length != 1)
+                return false;
+
+            var dimensionSize = operation.DimensionSizes[0].ConstantValue;
+            return dimensionSize.HasValue && (int)dimensionSize.Value == 0;
+        }
+
+        private static bool IsInAttribute(IArrayCreationOperation operation)
+        {
+            return operation.Syntax.Ancestors().OfType<AttributeArgumentSyntax>().Any();
+        }
+
+        private static bool IsCompilerGeneratedParamsArray(IArrayCreationOperation arrayCreationExpression, OperationAnalysisContext context)
+        {
+            var model = context.Compilation.GetSemanticModel(arrayCreationExpression.Syntax.SyntaxTree);
+
+            // Compiler generated array creation seems to just use the syntax from the parent.
+            var parent = model.GetOperation(arrayCreationExpression.Syntax, context.CancellationToken);
+            if (parent == null)
+                return false;
+
+            ISymbol targetSymbol = null;
+            var arguments = ImmutableArray<IArgumentOperation>.Empty;
+            if (parent is IInvocationOperation invocation)
+            {
+                targetSymbol = invocation.TargetMethod;
+                arguments = invocation.Arguments;
+            }
+            else
+            {
+                if (parent is IObjectCreationOperation objectCreation)
+                {
+                    targetSymbol = objectCreation.Constructor;
+                    arguments = objectCreation.Arguments;
+                }
+                else if (parent is IPropertyReferenceOperation propertyReference)
+                {
+                    targetSymbol = propertyReference.Property;
+                    arguments = propertyReference.Arguments;
+                }
+            }
+
+            if (targetSymbol == null)
+                return false;
+
+            var parameters = GetParameters(targetSymbol);
+            if (parameters.Length == 0 || !parameters[parameters.Length - 1].IsParams)
+                return false;
+
+            // At this point the array creation is known to be compiler synthesized as part of a call
+            // to a method with a params parameter, and so it is probably sound to return true at this point.
+            // As a sanity check, verify that the last argument to the call is equivalent to the array creation.
+            // (Comparing for object identity does not work because the semantic model can return a fresh operation tree.)
+            var lastArgument = arguments.LastOrDefault();
+            return lastArgument != null && lastArgument.Value.Syntax == arrayCreationExpression.Syntax;
+        }
+
+        private static ImmutableArray<IParameterSymbol> GetParameters(ISymbol symbol)
+        {
+            switch (symbol.Kind)
+            {
+                case SymbolKind.Method:
+                    return ((IMethodSymbol)symbol).Parameters;
+                case SymbolKind.Property:
+                    return ((IPropertySymbol)symbol).Parameters;
+                default:
+                    return ImmutableArray<IParameterSymbol>.Empty;
             }
         }
     }
