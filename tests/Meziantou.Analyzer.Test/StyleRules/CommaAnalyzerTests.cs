05ebf1cad84e2a0cd9d00c8288478bef1f862f4c
meziantou <meziantou@live.fr>
2019-02-01 17:14:34 -0500

    Add analyzer MA0007

---
+++ tests/Meziantou.Analyzer.Test/StyleRules/CommaAnalyzerTests.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestHelper;
+
+namespace Meziantou.Analyzer.Test.StyleRules
+{
+    [TestClass]
+    public class CommaAnalyzerTests : CodeFixVerifier
+    {
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() => new CommaAnalyzer();
+
+        [TestMethod]
+        public void EmptyString_ShouldNotReportDiagnosticForEmptyString()
+        {
+            var test = "";
+            VerifyCSharpDiagnostic(test);
+        }
+
+        [TestMethod]
+        public void OneLineDeclarationWithMissingTrailingComma_ShouldNotReportDiagnostic()
+        {
+            var test = @"
+class TypeName
+{
+    public int A { get; set; }
+    public int B { get; set; }
+
+    public async System.Threading.Tasks.Task Test()
+    {
+        new TypeName() { A = 1 };
+    }
+}";
+
+            VerifyCSharpDiagnostic(test);
+        }
+
+        [TestMethod]
+        public void MultipleLinesDeclarationWithTrailingComma_ShouldNotReportDiagnostic()
+        {
+            var test = @"
+class TypeName
+{
+    public int A { get; set; }
+    public int B { get; set; }
+
+    public async System.Threading.Tasks.Task Test()
+    {
+        new TypeName()
+        {
+            A = 1,
+            B = 2,
+        };
+    }
+}";
+
+            VerifyCSharpDiagnostic(test);
+        }
+
+        [TestMethod]
+        public void MultipleLinesDeclarationWithMissingTrailingComma_ShouldReportDiagnostic()
+        {
+            var test = @"
+class TypeName
+{
+    public int A { get; set; }
+    public int B { get; set; }
+
+    public async System.Threading.Tasks.Task Test()
+    {
+        new TypeName()
+        {
+            A = 1,
+            B = 2
+        };
+    }
+}";
+
+            var expected = new DiagnosticResult
+            {
+                Id = "MA0007",
+                Message = "Add comma after the last property",
+                Severity = DiagnosticSeverity.Info,
+                Locations = new[]
+                {
+                    new DiagnosticResultLocation("Test0.cs", line: 12, column: 13)
+                }
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+        }
+    }
+}
