d2b480483f5fe34f08978fbb2d2e130a318b26ed
Gérald Barré <meziantou@live.fr>
2019-02-01 22:35:45 -0500

    Add MA0008

---
+++ src/Meziantou.Analyzer/UsageRules/UseStructLayoutAttributeAnalyzer.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Meziantou.Analyzer
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class UseStructLayoutAttributeAnalyzer : DiagnosticAnalyzer
+    {
+        private static readonly DiagnosticDescriptor s_rule = new DiagnosticDescriptor(
+            RuleIdentifiers.MissingStructLayoutAttribute,
+            title: "Add StructLayoutAttribute",
+            messageFormat: "Add StructLayoutAttribute",
+            RuleCategories.Usage,
+            DiagnosticSeverity.Warning,
+            isEnabledByDefault: true,
+            description: "",
+            helpLinkUri: RuleIdentifiers.GetHelpUri(RuleIdentifiers.MissingStructLayoutAttribute));
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(s_rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+
+            context.RegisterCompilationStartAction(compilationContext =>
+            {
+                var attributeType = compilationContext.Compilation.GetTypeByMetadataName("System.Runtime.InteropServices.StructLayoutAttribute");
+                if (attributeType == null)
+                    return;
+
+                compilationContext.RegisterSyntaxNodeAction(Analyze, SyntaxKind.StructDeclaration);
+            });
+        }
+
+        private void Analyze(SyntaxNodeAnalysisContext context)
+        {
+            var declaration = (StructDeclarationSyntax)context.Node;
+            if (declaration == null)
+                return;
+
+            var attributeType = context.Compilation.GetTypeByMetadataName("System.Runtime.InteropServices.StructLayoutAttribute");
+            if (attributeType == null)
+                return;
+
+            var typeInfo = context.SemanticModel.GetDeclaredSymbol(declaration);
+            if (typeInfo == null)
+                return;
+
+            if (typeInfo.GetAttributes().Any(attr => attributeType.Equals(attr.AttributeClass)))
+                return;
+
+            context.ReportDiagnostic(Diagnostic.Create(s_rule, declaration.GetLocation()));
+        }
+    }
+}
---
+++ tests/Meziantou.Analyzer.Test/UsageRules/UseStructLayoutAttributeAnalyzerTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Meziantou.Analyzer.UsageRules;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestHelper;
+
+namespace Meziantou.Analyzer.Test.UsageRules
+{
+    [TestClass]
+    public class UseStructLayoutAttributeAnalyzerTests : CodeFixVerifier
+    {
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() => new UseStructLayoutAttributeAnalyzer();
+
+        [TestMethod]
+        public void ShouldNotReportDiagnosticForEmptyString()
+        {
+            var test = "";
+            VerifyCSharpDiagnostic(test);
+        }
+
+        [TestMethod]
+        public void MissingAttribute_ShouldReportDiagnostic()
+        {
+            var test = "struct TypeName { }";
+            var expected = new DiagnosticResult
+            {
+                Id = "MA0008",
+                Message = "Add StructLayoutAttribute",
+                Severity = DiagnosticSeverity.Warning,
+                Locations = new[]
+                {
+                    new DiagnosticResultLocation("Test0.cs", line: 1, column: 0)
+                }
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+        }
+
+        [TestMethod]
+        public void WithAttribute_ShouldNotReportDiagnostic()
+        {
+            var test = @"using System.Runtime.InteropServices;
+[StructLayout(LayoutKind.Sequential)]
+struct TypeName
+{
+}";
+            
+            VerifyCSharpDiagnostic(test);
+        }
+    }
+}
