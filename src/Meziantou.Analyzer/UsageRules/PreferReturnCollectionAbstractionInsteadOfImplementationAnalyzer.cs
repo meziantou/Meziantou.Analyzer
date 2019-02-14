6441ae56456d208519035978b82e718a2ddb3310
meziantou <meziantou@live.fr>
2019-02-14 17:08:14 -0500

    update ma0016

--- src/Meziantou.Analyzer/UsageRules/PreferReturnCollectionAbstractionInsteadOfImplementationAnalyzer.cs
+++ src/Meziantou.Analyzer/UsageRules/PreferReturnCollectionAbstractionInsteadOfImplementationAnalyzer.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Text;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -29,11 +28,14 @@ namespace Meziantou.Analyzer.UsageRules
             context.EnableConcurrentExecution();
             context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
 
-            context.RegisterSyntaxNodeAction(AnalyzeMethod, SyntaxKind.MethodDeclaration);
-            context.RegisterSyntaxNodeAction(AnalyzeProperty, SyntaxKind.PropertyDeclaration);
-            context.RegisterSyntaxNodeAction(AnalyzeIndexer, SyntaxKind.IndexerDeclaration);
             context.RegisterSyntaxNodeAction(AnalyzeDelegate, SyntaxKind.DelegateDeclaration);
             context.RegisterSyntaxNodeAction(AnalyzeField, SyntaxKind.FieldDeclaration);
+            context.RegisterSyntaxNodeAction(AnalyzeIndexer, SyntaxKind.IndexerDeclaration);
+            context.RegisterSyntaxNodeAction(AnalyzeMethod, SyntaxKind.MethodDeclaration);
+            context.RegisterSyntaxNodeAction(AnalyzeProperty, SyntaxKind.PropertyDeclaration);
+            // TODO method parameters
+            // TODO delegate parameters
+            // TODO indexer parameter
         }
 
         private void AnalyzeField(SyntaxNodeAnalysisContext context)
@@ -59,20 +61,94 @@ namespace Meziantou.Analyzer.UsageRules
             context.ReportDiagnostic(Diagnostic.Create(s_rule, node.GetLocation()));
         }
 
-        private void AnalyzeDelegate(SyntaxNodeAnalysisContext obj)
+        private void AnalyzeDelegate(SyntaxNodeAnalysisContext context)
         {
+            var node = (DelegateDeclarationSyntax)context.Node;
+            if (node == null)
+                return;
+
+            if (!IsVisible(node.Modifiers))
+                return;
+
+            var type = node.ReturnType;
+            if (type != null && !IsValidType(context.Compilation, context.SemanticModel.GetTypeInfo(type).Type))
+            {
+                context.ReportDiagnostic(Diagnostic.Create(s_rule, node.GetLocation()));
+            }
+
+            AnalyzeParameters(context, node.ParameterList?.Parameters);
         }
 
-        private void AnalyzeIndexer(SyntaxNodeAnalysisContext obj)
+        private void AnalyzeIndexer(SyntaxNodeAnalysisContext context)
         {
+            var node = (IndexerDeclarationSyntax)context.Node;
+            if (node == null)
+                return;
+
+            if (!IsVisible(node.Modifiers))
+                return;
+
+            var type = node.Type;
+            if (type != null && !IsValidType(context.Compilation, context.SemanticModel.GetTypeInfo(type).Type))
+            {
+                context.ReportDiagnostic(Diagnostic.Create(s_rule, node.GetLocation()));
+            }
+
+            AnalyzeParameters(context, node.ParameterList?.Parameters);
         }
 
-        private void AnalyzeProperty(SyntaxNodeAnalysisContext obj)
+        private void AnalyzeProperty(SyntaxNodeAnalysisContext context)
         {
+            var node = (PropertyDeclarationSyntax)context.Node;
+            if (node == null)
+                return;
+
+            if (!IsVisible(node.Modifiers))
+                return;
+
+            var type = node.Type;
+            if (type == null || IsValidType(context.Compilation, context.SemanticModel.GetTypeInfo(type).Type))
+                return;
+
+            context.ReportDiagnostic(Diagnostic.Create(s_rule, node.GetLocation()));
         }
 
-        private void AnalyzeMethod(SyntaxNodeAnalysisContext obj)
+        private void AnalyzeMethod(SyntaxNodeAnalysisContext context)
         {
+            var node = (MethodDeclarationSyntax)context.Node;
+            if (node == null)
+                return;
+
+            if (!IsVisible(node.Modifiers))
+                return;
+
+            var type = node.ReturnType;
+            if (type != null && !IsValidType(context.Compilation, context.SemanticModel.GetTypeInfo(type).Type))
+            {
+                context.ReportDiagnostic(Diagnostic.Create(s_rule, node.GetLocation()));
+            }
+
+            AnalyzeParameters(context, node.ParameterList?.Parameters);
+        }
+
+        private void AnalyzeParameters(SyntaxNodeAnalysisContext context, IEnumerable<ParameterSyntax> parameters)
+        {
+            if (parameters != null)
+            {
+                foreach (var parameter in parameters)
+                {
+                    AnalyzeParameter(context, parameter);
+                }
+            }
+        }
+
+        private void AnalyzeParameter(SyntaxNodeAnalysisContext context, ParameterSyntax parameter)
+        {
+            var type = parameter.Type;
+            if (type != null && !IsValidType(context.Compilation, context.SemanticModel.GetTypeInfo(type).Type))
+            {
+                context.ReportDiagnostic(Diagnostic.Create(s_rule, parameter.GetLocation()));
+            }
         }
 
         private static bool IsVisible(SyntaxTokenList modifiers)
