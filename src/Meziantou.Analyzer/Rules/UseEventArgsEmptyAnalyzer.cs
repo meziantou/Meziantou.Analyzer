bacf57f817dbacb259a36a53b284a8843b3645b4
meziantou <meziantou@live.fr>
2019-02-16 12:47:02 -0500

    Add MA0019

---
+++ src/Meziantou.Analyzer/Rules/UseEventArgsEmptyAnalyzer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace Meziantou.Analyzer.Rules
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class UseEventArgsEmptyAnalyzer : DiagnosticAnalyzer
+    {
+        private static readonly DiagnosticDescriptor s_rule = new DiagnosticDescriptor(
+            RuleIdentifiers.UseEventArgsEmpty,
+            title: "Use EventArgs.Empty",
+            messageFormat: "Use EventArgs.Empty instead of new EventArgs()",
+            RuleCategories.Usage,
+            DiagnosticSeverity.Warning,
+            isEnabledByDefault: true,
+            description: "",
+            helpLinkUri: RuleIdentifiers.GetHelpUri(RuleIdentifiers.UseEventArgsEmpty));
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(s_rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+
+            context.RegisterOperationAction(Analyze, OperationKind.ObjectCreation);
+        }
+
+        private void Analyze(OperationAnalysisContext context)
+        {
+            var operation = (IObjectCreationOperation)context.Operation;
+            if (operation == null)
+                return;
+
+            if (operation.Arguments.Length > 0)
+                return;
+
+            var type = context.Compilation.GetTypeByMetadataName("System.EventArgs");
+            if (operation.Constructor.ContainingType.IsEqualsTo(type))
+            {
+                context.ReportDiagnostic(Diagnostic.Create(s_rule, operation.Syntax.GetLocation()));
+            }
+        }
+    }
+}
---
+++ tests/Meziantou.Analyzer.Test/Rules/UseEventArgsEmptyAnalyzerTests.cs
@@ -0,0 +1,33 @@
+﻿using Meziantou.Analyzer.Rules;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestHelper;
+
+namespace Meziantou.Analyzer.Test.Rules
+{
+    [TestClass]
+    public class UseEventArgsEmptyAnalyzerTests : CodeFixVerifier
+    {
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() => new UseEventArgsEmptyAnalyzer();
+        protected override string ExpectedDiagnosticId => "MA0019";
+        protected override string ExpectedDiagnosticMessage => "Use EventArgs.Empty instead of new EventArgs()";
+        protected override DiagnosticSeverity ExpectedDiagnosticSeverity => DiagnosticSeverity.Warning;
+
+        [TestMethod]
+        public void ShouldReport()
+        {
+            var project = new ProjectBuilder()
+                .WithSource(@"
+class TypeName
+{
+    public void Test()
+    {
+        new System.EventArgs();
+    }
+}");
+            var expected = CreateDiagnosticResult(line: 6, column: 9);
+            VerifyDiagnostic(project, expected);
+        }
+    }
+}
