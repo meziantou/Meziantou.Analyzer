751fed090a0bcc17832963ffcade0a83d7b6a3c0
meziantou <meziantou@live.fr>
2019-02-20 13:40:53 -0500

    Add MA0023

--- src/Meziantou.Analyzer/Rules/UseRegexTimeoutAnalyzer.cs
+++ src/Meziantou.Analyzer/Rules/UseRegexTimeoutAnalyzer.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
+using System.Text.RegularExpressions;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.CodeAnalysis.Operations;
@@ -12,7 +14,7 @@ namespace Meziantou.Analyzer.Rules
     {
         private static readonly string[] s_methodNames = { "IsMatch", "Match", "Matches", "Replace", "Split" };
 
-        private static readonly DiagnosticDescriptor s_rule = new DiagnosticDescriptor(
+        private static readonly DiagnosticDescriptor s_timeoutRule = new DiagnosticDescriptor(
             RuleIdentifiers.MissingTimeoutParameterForRegex,
             title: "Add timeout parameter",
             messageFormat: "Add timeout parameter",
@@ -20,9 +22,19 @@ namespace Meziantou.Analyzer.Rules
             DiagnosticSeverity.Warning,
             isEnabledByDefault: true,
             description: "",
-            helpLinkUri: RuleIdentifiers.GetHelpUri(RuleIdentifiers.MissingStructLayoutAttribute));
+            helpLinkUri: RuleIdentifiers.GetHelpUri(RuleIdentifiers.MissingTimeoutParameterForRegex));
 
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(s_rule);
+        private static readonly DiagnosticDescriptor s_explicitCaptureRule = new DiagnosticDescriptor(
+            RuleIdentifiers.UseRegexExplicitCaptureOptions,
+            title: "Add RegexOptions.ExplicitCapture",
+            messageFormat: "Add RegexOptions.ExplicitCapture",
+            RuleCategories.Security,
+            DiagnosticSeverity.Warning,
+            isEnabledByDefault: true,
+            description: "",
+            helpLinkUri: RuleIdentifiers.GetHelpUri(RuleIdentifiers.UseRegexExplicitCaptureOptions));
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(s_timeoutRule, s_explicitCaptureRule);
 
         public override void Initialize(AnalysisContext context)
         {
@@ -42,25 +54,22 @@ namespace Meziantou.Analyzer.Rules
             if (!op.TargetMethod.IsStatic)
                 return;
 
-            var regexType = context.Compilation.GetTypeByMetadataName("System.Text.RegularExpressions.Regex");
-            var timespanType = context.Compilation.GetTypeByMetadataName("System.TimeSpan");
-            if (regexType == null || timespanType == null)
-                return;
-
             if (!s_methodNames.Contains(op.TargetMethod.Name, StringComparer.Ordinal))
                 return;
 
-            if (!regexType.Equals(op.TargetMethod.ContainingSymbol))
+            if (!op.TargetMethod.ContainingType.IsEqualsTo(context.Compilation.GetTypeByMetadataName("System.Text.RegularExpressions.Regex")))
                 return;
 
             if (op.Arguments.Length == 0)
                 return;
 
             var arg = op.Arguments.Last();
-            if (arg.Value == null || timespanType.Equals(arg.Value.Type))
-                return;
+            if (arg.Value == null || !arg.Value.Type.IsEqualsTo(context.Compilation.GetTypeByMetadataName("System.TimeSpan")))
+            {
+                context.ReportDiagnostic(Diagnostic.Create(s_timeoutRule, op.Syntax.GetLocation()));
+            }
 
-            context.ReportDiagnostic(Diagnostic.Create(s_rule, op.Syntax.GetLocation()));
+            CheckRegexOptionsArgument(context, op.Arguments, context.Compilation.GetTypeByMetadataName("System.Text.RegularExpressions.RegexOptions"));
         }
 
         private void AnalyzeObjectCreation(OperationAnalysisContext context)
@@ -69,21 +78,37 @@ namespace Meziantou.Analyzer.Rules
             if (op == null)
                 return;
 
-            var regexType = context.Compilation.GetTypeByMetadataName("System.Text.RegularExpressions.Regex");
-            var timespanType = context.Compilation.GetTypeByMetadataName("System.TimeSpan");
-            if (regexType == null || timespanType == null)
-                return;
-
             if (op.Arguments.Length == 0)
                 return;
 
-            if (!op.Type.IsEqualsTo(regexType))
+            if (!op.Type.IsEqualsTo(context.Compilation.GetTypeByMetadataName("System.Text.RegularExpressions.Regex")))
                 return;
 
-            if (op.Arguments.Last().Value.Type.IsEqualsTo(timespanType))
+            if (!op.Arguments.Last().Value.Type.IsEqualsTo(context.Compilation.GetTypeByMetadataName("System.TimeSpan")))
+            {
+                context.ReportDiagnostic(Diagnostic.Create(s_timeoutRule, op.Syntax.GetLocation()));
+            }
+
+            CheckRegexOptionsArgument(context, op.Arguments, context.Compilation.GetTypeByMetadataName("System.Text.RegularExpressions.RegexOptions"));
+        }
+
+        private void CheckRegexOptionsArgument(OperationAnalysisContext context, IEnumerable<IArgumentOperation> arguments, ITypeSymbol regexOptionsSymbol)
+        {
+            if (regexOptionsSymbol == null)
                 return;
 
-            context.ReportDiagnostic(Diagnostic.Create(s_rule, op.Syntax.GetLocation()));
+            var arg = arguments.FirstOrDefault(a => a.Parameter.Type.IsEqualsTo(regexOptionsSymbol));
+            if (arg == null || arg.Value == null)
+                return;
+
+            if (arg.Value.ConstantValue.HasValue)
+            {
+                var value = ((RegexOptions)arg.Value.ConstantValue.Value);
+                if (!value.HasFlag(RegexOptions.ExplicitCapture))
+                {
+                    context.ReportDiagnostic(Diagnostic.Create(s_explicitCaptureRule, arg.Syntax.GetLocation()));
+                }
+            }
         }
     }
 }
---
+++ tests/Meziantou.Analyzer.Test/Rules/UseRegexOptionsAnalyzerTests.cs
@@ -0,0 +1,56 @@
+﻿using System.Text.RegularExpressions;
+using Meziantou.Analyzer.Rules;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestHelper;
+
+namespace Meziantou.Analyzer.Test.Rules
+{
+    [TestClass]
+    public class UseRegexOptionsAnalyzerTests : CodeFixVerifier
+    {
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() => new UseRegexTimeoutAnalyzer();
+        protected override string ExpectedDiagnosticId => "MA0023";
+        protected override string ExpectedDiagnosticMessage => "Add RegexOptions.ExplicitCapture";
+        protected override DiagnosticSeverity ExpectedDiagnosticSeverity => DiagnosticSeverity.Warning;
+
+        [TestMethod]
+        public void IsMatch_RegexOptions()
+        {
+            var project = new ProjectBuilder()
+                  .AddReference(typeof(Regex))
+                  .WithSource(@"using System.Text.RegularExpressions;
+class TestClass
+{
+    void Test()
+    {
+        Regex.IsMatch(""test"", ""[a-z]+"", RegexOptions.CultureInvariant | RegexOptions.IgnoreCase, default);
+        Regex.IsMatch(""test"", ""[a-z]+"", RegexOptions.ExplicitCapture, default);
+    }
+}");
+
+            var expected = CreateDiagnosticResult(line: 6, column: 41);
+            VerifyDiagnostic(project, expected);
+        }
+
+        [TestMethod]
+        public void Ctor_RegexOptions()
+        {
+            var project = new ProjectBuilder()
+                  .AddReference(typeof(Regex))
+                  .WithSource(@"using System.Text.RegularExpressions;
+class TestClass
+{
+    void Test()
+    {
+        new Regex(""[a-z]+"", RegexOptions.CultureInvariant | RegexOptions.IgnoreCase, default);
+        new Regex(""[a-z]+"", RegexOptions.ExplicitCapture | RegexOptions.IgnoreCase, default);
+    }
+}");
+
+            var expected = CreateDiagnosticResult(line: 6, column: 29);
+            VerifyDiagnostic(project, expected);
+        }
+    }
+}
