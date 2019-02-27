26784509c9d23f226db1f0a06c27e15a078e24fc
meziantou <meziantou@live.fr>
2019-02-27 13:42:56 -0500

    Add MA0024

---
+++ src/Meziantou.Analyzer/Rules/DoNotUseEqualityComparerDefaultOfStringAnalyzer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace Meziantou.Analyzer.Rules
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class DoNotUseEqualityComparerDefaultOfStringAnalyzer : DiagnosticAnalyzer
+    {
+        private static readonly DiagnosticDescriptor s_rule = new DiagnosticDescriptor(
+            RuleIdentifiers.DoNotUseEqualityComparerDefaultOfString,
+            title: "Use StringComparer.Ordinal",
+            messageFormat: "Use StringComparer.Ordinal",
+            RuleCategories.Usage,
+            DiagnosticSeverity.Warning,
+            isEnabledByDefault: true,
+            description: "",
+            helpLinkUri: RuleIdentifiers.GetHelpUri(RuleIdentifiers.DoNotUseEqualityComparerDefaultOfString));
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(s_rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+
+            var values = Enum.GetValues(typeof(OperationKind)).Cast<OperationKind>().ToArray();
+
+            context.RegisterOperationAction(Analyze, OperationKind.PropertyReference);
+        }
+
+        private void Analyze(OperationAnalysisContext context)
+        {
+            var operation = (IPropertyReferenceOperation)context.Operation;
+            if (!string.Equals(operation.Member.Name, nameof(EqualityComparer<string>.Default), StringComparison.Ordinal))
+                return;
+
+            var equalityComparerSymbol = context.Compilation.GetTypeByMetadataName("System.Collections.Generic.EqualityComparer`1");
+            if (equalityComparerSymbol == null)
+                return;
+
+            var equalityComparerStringSymbol = equalityComparerSymbol.Construct(context.Compilation.GetSpecialType(SpecialType.System_String));
+            if (operation.Member.ContainingType.IsEqualsTo(equalityComparerStringSymbol))
+            {
+                context.ReportDiagnostic(Diagnostic.Create(s_rule, operation.Syntax.GetLocation()));
+            }
+        }
+    }
+}
---
+++ tests/Meziantou.Analyzer.Test/Rules/DoNotUseEqualityComparerDefaultOfStringAnalyzerTests.cs
@@ -0,0 +1,39 @@
+﻿using Meziantou.Analyzer.Rules;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestHelper;
+
+namespace Meziantou.Analyzer.Test.Rules
+{
+    [TestClass]
+    public class DoNotUseEqualityComparerDefaultOfStringAnalyzerTests : CodeFixVerifier
+    {
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() => new DoNotUseEqualityComparerDefaultOfStringAnalyzer();
+        protected override string ExpectedDiagnosticId => "MA0024";
+        protected override string ExpectedDiagnosticMessage => "Use StringComparer.Ordinal";
+        protected override DiagnosticSeverity ExpectedDiagnosticSeverity => DiagnosticSeverity.Warning;
+
+        [TestMethod]
+        public void Test()
+        {
+            var project = new ProjectBuilder()
+                  .WithSource(@"using System.Collections.Generic;
+class Test
+{
+    internal void Sample()
+    {
+        _ = EqualityComparer<int>.Default.Equals(0, 0);
+        _ = EqualityComparer<string>.Default.Equals(null, null);
+    }
+}
+");
+
+            var expected = new[]
+            {
+                CreateDiagnosticResult(line: 7, column: 13),
+            };
+            VerifyDiagnostic(project, expected);
+        }
+    }
+}
