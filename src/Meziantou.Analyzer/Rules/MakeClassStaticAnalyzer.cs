d0b3fa31a01e01b79245482a13baed32774499b3
Gérald Barré <meziantou@live.fr>
2019-03-14 23:07:16 -0400

    add MA0037

---
+++ src/Meziantou.Analyzer/Rules/MakeClassStaticAnalyzer.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.FindSymbols;
+
+namespace Meziantou.Analyzer.Rules
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class MakeClassStaticAnalyzer : DiagnosticAnalyzer
+    {
+        private static readonly DiagnosticDescriptor s_rule = new DiagnosticDescriptor(
+            RuleIdentifiers.MakeClassStaticAnalyzer,
+            title: "Make class static",
+            messageFormat: "Make class static",
+            RuleCategories.Design,
+            DiagnosticSeverity.Info,
+            isEnabledByDefault: true,
+            description: "",
+            helpLinkUri: RuleIdentifiers.GetHelpUri(RuleIdentifiers.AbstractTypesShouldNotHaveConstructors));
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(s_rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+
+            context.RegisterCompilationStartAction(s =>
+            {
+                var potentialClasses = new List<(ClassDeclarationSyntax, ITypeSymbol)>();
+                var parentClasses = new List<ITypeSymbol>();
+
+                s.RegisterSyntaxNodeAction(Analyze, SyntaxKind.ClassDeclaration);
+                s.RegisterCompilationEndAction(context => End());
+            });
+        }
+
+        private void Analyze(SyntaxNodeAnalysisContext context)
+        {
+            var node = (ClassDeclarationSyntax)context.Node;
+            var classSymbol = context.SemanticModel.GetDeclaredSymbol(node, context.CancellationToken);
+            if (classSymbol == null || classSymbol.IsStatic)
+                return;
+
+            if (classSymbol.GetMembers().All(member => member.IsStatic))
+            {
+                context.ReportDiagnostic(Diagnostic.Create(s_rule, node.Identifier.GetLocation()));
+            }
+        }
+    }
+}
---
+++ tests/Meziantou.Analyzer.Test/Rules/MakeClassStaticAnalyzerTests.cs
@@ -0,0 +1,37 @@
+﻿using Meziantou.Analyzer.Rules;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestHelper;
+
+namespace Meziantou.Analyzer.Test.Rules
+{
+    [TestClass]
+    public class MakeClassStaticAnalyzerTests : CodeFixVerifier
+    {
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() => new MakeClassStaticAnalyzer();
+        protected override string ExpectedDiagnosticId => "MA0032";
+        protected override string ExpectedDiagnosticMessage => "Make class static";
+        protected override DiagnosticSeverity ExpectedDiagnosticSeverity => DiagnosticSeverity.Info;
+
+        [TestMethod]
+        public void NoDiagnostic()
+        {
+            var project = new ProjectBuilder()
+                  .WithSource(@"
+abstract class Test
+{
+    static void A() { }
+}
+
+class Test2
+{
+    static void A() { }
+}
+
+class Test : Test2 { }");
+
+            VerifyDiagnostic(project);
+        }
+    }
+}
