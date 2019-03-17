72529c3fab5a8d2183f4a2162a3e6148f856e3b0
meziantou <meziantou@live.fr>
2019-03-17 01:07:42 -0400

    Add MA0039

---
+++ src/Meziantou.Analyzer/Rules/DoNotUseServerCertificateValidationCallbackAnalyzer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace Meziantou.Analyzer.Rules
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class DoNotUseServerCertificateValidationCallbackAnalyzer : DiagnosticAnalyzer
+    {
+        private static readonly DiagnosticDescriptor s_rule = new DiagnosticDescriptor(
+            RuleIdentifiers.DoNotUseServerCertificateValidationCallback,
+            title: "Do not use ServerCertificateValidationCallback",
+            messageFormat: "Do not use ServerCertificateValidationCallback",
+            RuleCategories.Security,
+            DiagnosticSeverity.Error,
+            isEnabledByDefault: true,
+            description: "",
+            helpLinkUri: RuleIdentifiers.GetHelpUri(RuleIdentifiers.DoNotUseServerCertificateValidationCallback));
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(s_rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+
+            context.RegisterCompilationStartAction(ctx =>
+            {
+                var servicePointManagerSymbol = ctx.Compilation.GetTypeByMetadataName("System.Net.ServicePointManager");
+                if (servicePointManagerSymbol == null)
+                    return;
+
+                var eventSymbol = servicePointManagerSymbol.GetMembers("ServerCertificateValidationCallback").FirstOrDefault();
+                if (eventSymbol == null)
+                    return;
+
+                ctx.RegisterOperationAction(c => Analyze(c, eventSymbol), OperationKind.PropertyReference);
+            });
+        }
+
+        private void Analyze(OperationAnalysisContext context, ISymbol eventSymbol)
+        {
+            var operation = (IPropertyReferenceOperation)context.Operation;
+            if (operation.Property.Equals(eventSymbol))
+            {
+                context.ReportDiagnostic(Diagnostic.Create(s_rule, operation.Syntax.GetLocation()));
+            }
+        }
+    }
+}
---
+++ tests/Meziantou.Analyzer.Test/Rules/DoNotUseServerCertificateValidationCallbackAnalyzerTests.cs
@@ -0,0 +1,46 @@
+﻿using Meziantou.Analyzer.Rules;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestHelper;
+
+namespace Meziantou.Analyzer.Test.Rules
+{
+    [TestClass]
+    public class DoNotUseServerCertificateValidationCallbackAnalyzerTests : CodeFixVerifier
+    {
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() => new DoNotUseServerCertificateValidationCallbackAnalyzer();
+        protected override string ExpectedDiagnosticId => "MA0039";
+        protected override DiagnosticSeverity ExpectedDiagnosticSeverity => DiagnosticSeverity.Error;
+
+        [TestMethod]
+        public void StaticMembers()
+        {
+            var project = new ProjectBuilder()
+                  .WithSource(@"
+class Test
+{
+    void A()
+    {
+        System.Net.ServicePointManager.ServerCertificateValidationCallback += (sender, certification, chain, sslPolicyErrors) => throw null;
+    }
+}
+
+namespace System.Net
+{
+    public class ServicePointManager
+    {
+        public static System.Net.Security.RemoteCertificateValidationCallback ServerCertificateValidationCallback { get; set; }
+    }
+}
+
+namespace System.Net.Security
+{
+    public delegate bool RemoteCertificateValidationCallback(object sender, object certificate, object chain, object sslPolicyErrors);
+}
+");
+
+            VerifyDiagnostic(project, CreateDiagnosticResult(line: 6, column: 9));
+        }
+    }
+}
