b9bc40ecb900799e43ff384cd5a637b69f7204fd
Gérald Barré <meziantou@live.fr>
2019-03-27 23:06:32 -0400

    Add fixers

--- tests/Meziantou.Analyzer.Test/Rules/AbstractTypesShouldNotHaveConstructorsAnalyzerTests.cs
+++ tests/Meziantou.Analyzer.Test/Rules/AbstractTypesShouldNotHaveConstructorsAnalyzerTests.cs
@@ -1,5 +1,6 @@
 ﻿using Meziantou.Analyzer.Rules;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TestHelper;
@@ -10,6 +11,7 @@ namespace Meziantou.Analyzer.Test.Rules
     public class AbstractTypesShouldNotHaveConstructorsAnalyzerTests : CodeFixVerifier
     {
         protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() => new AbstractTypesShouldNotHaveConstructorsAnalyzer();
+        protected override CodeFixProvider GetCSharpCodeFixProvider() => new AbstractTypesShouldNotHaveConstructorsFixer();
         protected override string ExpectedDiagnosticId => "MA0017";
         protected override string ExpectedDiagnosticMessage => "Abstract types should not have public or internal constructors";
         protected override DiagnosticSeverity ExpectedDiagnosticSeverity => DiagnosticSeverity.Warning;
@@ -21,8 +23,6 @@ namespace Meziantou.Analyzer.Test.Rules
                   .WithSource(@"
 abstract class Test
 {
-    public Test() { } // Error
-    internal Test(long a) { } // Error
     protected Test(int a) { }
     private Test(object a) { }
 }
@@ -35,12 +35,43 @@ class Test2
     private Test2(object a) { }
 }");
 
-            var expected = new[]
-            {
-                CreateDiagnosticResult(line: 4, column: 5),
-                CreateDiagnosticResult(line: 5, column: 5),
-            };
-            VerifyDiagnostic(project, expected);
+            VerifyDiagnostic(project);
+        }
+
+        [TestMethod]
+        public void PublicCtor()
+        {
+            var project = new ProjectBuilder()
+                  .WithSource(@"
+abstract class Test
+{
+    public Test() { }
+}");
+
+            VerifyDiagnostic(project, CreateDiagnosticResult(line: 4, column: 5));
+            VerifyFix(project, @"
+abstract class Test
+{
+    protected Test() { }
+}");
+        }
+
+        [TestMethod]
+        public void InternalCtor()
+        {
+            var project = new ProjectBuilder()
+                  .WithSource(@"
+abstract class Test
+{
+    internal Test() { }
+}");
+
+            VerifyDiagnostic(project, CreateDiagnosticResult(line: 4, column: 5));
+            VerifyFix(project, @"
+abstract class Test
+{
+    protected Test() { }
+}");
         }
     }
 }
