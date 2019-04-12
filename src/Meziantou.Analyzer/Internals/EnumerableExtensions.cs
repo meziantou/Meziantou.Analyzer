f163dac5f3d78e55a2ba2dbd734ec0bb984d1d4f
meziantou <meziantou@live.fr>
2019-04-12 15:59:37 -0400

    Fix SingleOrDefault exception

---
+++ src/Meziantou.Analyzer/Internals/EnumerableExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Meziantou.Analyzer
+{
+    internal static class EnumerableExtensions
+    {
+        public static TSource SingleOrDefaultIfMultiple<TSource>(this IEnumerable<TSource> source)
+        {
+            var elements = source.Take(2).ToArray();
+
+            return (elements.Length == 1) ? elements[0] : default;
+        }
+    }
+}
