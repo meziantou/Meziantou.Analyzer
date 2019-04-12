d98e5c58bcab6653b0420e17b9588f491a203415
meziantou <meziantou@live.fr>
2019-04-12 16:55:02 -0400

    Add MA0047

---
+++ src/Meziantou.Analyzer/Rules/DeclareTypesInNamespacesAnalyzer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Meziantou.Analyzer.Rules
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class DeclareTypesInNamespacesAnalyzer : DiagnosticAnalyzer
+    {
+        private static readonly DiagnosticDescriptor s_rule = new DiagnosticDescriptor(
+            RuleIdentifiers.DeclareTypesInNamespaces,
+            title: "Declare types in namespaces",
+            messageFormat: "Declare types in namespaces",
+            RuleCategories.Design,
+            DiagnosticSeverity.Warning,
+            isEnabledByDefault: true,
+            description: "",
+            helpLinkUri: RuleIdentifiers.GetHelpUri(RuleIdentifiers.DeclareTypesInNamespaces));
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(s_rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+
+            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+        }
+
+        private static void AnalyzeSymbol(SymbolAnalysisContext context)
+        {
+            var symbol = (INamedTypeSymbol)context.Symbol;
+            if (symbol.ContainingType == null && (symbol.ContainingNamespace?.IsGlobalNamespace ?? true))
+            {
+                context.ReportDiagnostic(s_rule, symbol);
+            }
+        }
+    }
+}
---
+++ tests/Meziantou.Analyzer.Test/Rules/DeclareTypesInNamespacesAnalyzerTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Tasks;
+using Meziantou.Analyzer.Rules;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestHelper;
+
+namespace Meziantou.Analyzer.Test.Rules
+{
+    [TestClass]
+    public class DeclareTypesInNamespacesAnalyzerTests
+    {
+        private static ProjectBuilder CreateProjectBuilder()
+        {
+            return new ProjectBuilder()
+                .WithAnalyzer<DeclareTypesInNamespacesAnalyzer>();
+        }
+
+        [TestMethod]
+        public async Task InNamespace_IsValid()
+        {
+            await CreateProjectBuilder()
+                  .WithSourceCode(@"
+namespace Test
+{
+    class Sample
+    {
+    }
+}")
+                  .ShouldNotReportDiagnostic()
+                  .ValidateAsync();
+        }
+
+        [TestMethod]
+        public async Task NotInNamespace_IsInvalid()
+        {
+            await CreateProjectBuilder()
+                  .WithSourceCode(@"
+class Sample
+{
+    class Nested { }
+}")
+                  .ShouldReportDiagnostic(line: 2, column: 7)
+                  .ValidateAsync();
+        }
+    }
+}
