346aa0908ded0f24a5afdaf2f66ff177528b1976
meziantou <meziantou@live.fr>
2019-04-16 17:43:14 -0400

    Add MA0048

---
+++ tests/Meziantou.Analyzer.Test/Rules/FileNameMustMatchTypeNameAnalyzerTests.cs
@@ -0,0 +1,77 @@
+﻿using System.Threading.Tasks;
+using Meziantou.Analyzer.Rules;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestHelper;
+
+namespace Meziantou.Analyzer.Test.Rules
+{
+    [TestClass]
+    public class FileNameMustMatchTypeNameAnalyzerTests
+    {
+        private static ProjectBuilder CreateProjectBuilder()
+        {
+            return new ProjectBuilder()
+                .WithAnalyzer<FileNameMustMatchTypeNameAnalyzer>();
+        }
+
+        [TestMethod]
+        public async Task DoesNotMatchFileName()
+        {
+            await CreateProjectBuilder()
+                  .WithSourceCode(@"
+class Sample
+{
+}")
+                  .ShouldReportDiagnostic(line: 2, column: 7)
+                  .ValidateAsync();
+        }
+        
+        [TestMethod]
+        public async Task DoesMatchFileName()
+        {
+            await CreateProjectBuilder()
+                  .WithSourceCode(@"
+class Test0
+{
+}")
+                  .ShouldNotReportDiagnostic()
+                  .ValidateAsync();
+        }
+        
+        [TestMethod]
+        public async Task DoesMatchFileName_Generic()
+        {
+            await CreateProjectBuilder()
+                  .WithSourceCode(@"
+class Test0<T>
+{
+}")
+                  .ShouldNotReportDiagnostic()
+                  .ValidateAsync();
+        }
+
+        [TestMethod]
+        public async Task DoesMatchFileName_GenericUsingArity()
+        {
+            await CreateProjectBuilder()
+                  .WithSourceCode(fileName: "Test0`1.cs", @"
+class Test0<T>
+{
+}")
+                  .ShouldNotReportDiagnostic()
+                  .ValidateAsync();
+        }
+        
+        [TestMethod]
+        public async Task DoesMatchFileName_GenericUsingOfT()
+        {
+            await CreateProjectBuilder()
+                  .WithSourceCode(fileName: "Test0OfT.cs", @"
+class Test0<T>
+{
+}")
+                  .ShouldNotReportDiagnostic()
+                  .ValidateAsync();
+        }
+    }
+}
