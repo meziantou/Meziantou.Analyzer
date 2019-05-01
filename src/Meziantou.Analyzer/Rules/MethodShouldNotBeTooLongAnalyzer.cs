86d0086d84c6036d6bc59aace780854392fcc0ca
meziantou <meziantou@live.fr>
2019-04-30 20:40:11 -0400

    Detect methods with too many statements (#22)

---
+++ src/Meziantou.Analyzer/Rules/MethodShouldNotBeTooLongAnalyzer.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Meziantou.Analyzer.Configurations;
+using Microsoft.CodeAnalysis.CSharp;
+using System.Globalization;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Linq;
+
+namespace Meziantou.Analyzer.Rules
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class MethodShouldNotBeTooLongAnalyzer : DiagnosticAnalyzer
+    {
+        private static readonly DiagnosticDescriptor s_rule = new DiagnosticDescriptor(
+            RuleIdentifiers.MethodShouldNotBeTooLong,
+            title: "Method is too long",
+            messageFormat: "Method is too long ({0} statements; maximum: {1})",
+            RuleCategories.Design,
+            DiagnosticSeverity.Warning,
+            isEnabledByDefault: true,
+            description: "",
+            helpLinkUri: RuleIdentifiers.GetHelpUri(RuleIdentifiers.MethodShouldNotBeTooLong));
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(s_rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+
+            context.RegisterSyntaxNodeAction(AnalyzeMethod, SyntaxKind.MethodDeclaration);
+        }
+
+        private static void AnalyzeMethod(SyntaxNodeAnalysisContext context)
+        {
+            var maximumStatements = GetMaximumNumberOfStatements(context);
+            if (maximumStatements <= 0)
+                return;
+
+            var node = (MethodDeclarationSyntax)context.Node;
+            if (node.Body != null)
+            {
+                var statements = CountStatements(node.Body);
+                if (statements > maximumStatements)
+                {
+                    context.ReportDiagnostic(s_rule, node.Identifier, statements.ToString(CultureInfo.InvariantCulture), maximumStatements.ToString(CultureInfo.InvariantCulture));
+                }
+            }
+        }
+
+        // internal for testing
+        internal static int CountStatements(BlockSyntax block)
+        {
+#if DEBUG
+            var statements = block.DescendantNodes().OfType<StatementSyntax>().ToList();
+#endif
+            return block.DescendantNodes(descendIntoChildren: ShouldDescendIntoChildren)
+                .OfType<StatementSyntax>()
+                .Where(IsCountableStatement)
+                .Count();
+
+            bool ShouldDescendIntoChildren(SyntaxNode node)
+            {
+                if (node is LocalFunctionStatementSyntax)
+                    return false;
+
+                return true;
+            }
+
+            bool IsCountableStatement(StatementSyntax statement)
+            {
+                if (statement is BlockSyntax || statement is LocalFunctionStatementSyntax)
+                    return false;
+
+                return true;
+            }
+        }
+
+        private static int GetMaximumNumberOfStatements(SyntaxNodeAnalysisContext context)
+        {
+            var file = context.Node.SyntaxTree.FilePath;
+            if (context.Options.TryGetConfigurationValue(file, "meziantou.maximumStatementsPerMethod", out var value) && int.TryParse(value, NumberStyles.Integer, CultureInfo.InvariantCulture, out var maxStatements))
+                return maxStatements;
+
+            return -1;
+        }
+    }
+}
---
+++ tests/Meziantou.Analyzer.Test/Rules/MethodShouldNotBeTooLongAnalyzerTests.cs
@@ -0,0 +1,90 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Meziantou.Analyzer.Rules;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestHelper;
+
+namespace Meziantou.Analyzer.Test.Rules
+{
+    [TestClass]
+    public class MethodShouldNotBeTooLongAnalyzerTests
+    {
+        private static ProjectBuilder CreateProjectBuilder()
+        {
+            return new ProjectBuilder()
+                .WithAnalyzer<MethodShouldNotBeTooLongAnalyzer>();
+        }
+
+        [TestMethod]
+        public async Task TooLongMethod()
+        {
+            const string SourceCode = @"
+public class Test
+{
+    void [|]Method()
+    {
+        var a = 0;
+        var b = 0;
+        var c = 0;
+    }
+}";
+            await CreateProjectBuilder()
+                  .WithSourceCode(SourceCode)
+                  .WithEditorConfig("meziantou.maximumStatementsPerMethod=2")
+                  .ValidateAsync();
+        }
+
+        [TestMethod]
+        public void CountStatement_ForLoop()
+        {
+            const string SourceCode = @"
+for (int a = 0; i < 0; i++)
+{
+    throw null;
+}";
+
+            var count = CountStatements(SourceCode);
+            Assert.AreEqual(2, count);
+        }
+
+        [TestMethod]
+        public void CountStatement_LocalFunction()
+        {
+            const string SourceCode = @"
+throw null;
+
+void B()
+{
+    throw null;
+}
+";
+
+            var count = CountStatements(SourceCode);
+            Assert.AreEqual(1, count);
+        }
+
+        [TestMethod]
+        public void CountStatement_If()
+        {
+            const string SourceCode = @"
+if (true)
+{
+    throw null;
+}
+";
+
+            var count = CountStatements(SourceCode);
+            Assert.AreEqual(2, count);
+        }
+
+        private static int CountStatements(string code)
+        {
+            var tree = CSharpSyntaxTree.ParseText("void A(){ " + code + " }");
+
+            var root = tree.GetRoot().DescendantNodes().OfType<BlockSyntax>().First();
+            return MethodShouldNotBeTooLongAnalyzer.CountStatements(root);
+        }
+    }
+}
