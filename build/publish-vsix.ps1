f3a0f6ec4c8892f37d1f35b8c78a07fa92f9422c
meziantou <meziantou@live.fr>
2019-05-17 19:16:18 -0400

    update ci

--- build/publish-vsix.ps1
+++ build/publish-vsix.ps1
@@ -1,6 +1,6 @@
 ﻿$PersonalAccessToken = $args[0]
-$VsixPath = "src\Meziantou.Analyzer.Vsix\bin\Release\Meziantou.Analyzer.vsix"
-$ManifestPath = "src\Meziantou.Analyzer.Vsix\bin\Release\extension-manifest.json"
+$VsixPath = "$PSScriptRoot\..\src\Meziantou.Analyzer.Vsix\bin\Release\Meziantou.Analyzer.vsix"
+$ManifestPath = "$PSScriptRoot\extension-manifest.json"
 
 $Installation = & "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -prerelease -format json | ConvertFrom-Json
 $Path = $Installation.installationPath
