581cf00df39c01e84b91484f01a21c9e3fd5dc1d
Gérald Barré <meziantou@users.noreply.github.com>
2019-05-16 22:51:07 -0400

    Publish artifacts (#35)

---
+++ update-version.ps1
@@ -0,0 +1,22 @@
+﻿$VerbosePreference="Continue"
+
+$version = $args[0]
+if (!$version) {
+    $version = "0.0.0"
+}
+
+Write-Host "Version: $version"
+
+# Update NuGet package version
+$FullPath = Resolve-Path $PSScriptRoot\src\Meziantou.Analyzer\Meziantou.Analyzer.csproj
+Write-Host $FullPath
+[xml]$content = Get-Content $FullPath
+$content.Project.PropertyGroup.PackageVersion = $version
+$content.Save($FullPath)
+
+# Update VSIX version
+$FullPath = Resolve-Path $PSScriptRoot\src\Meziantou.Analyzer.Vsix\source.extension.vsixmanifest
+Write-Host $FullPath
+[xml]$content = Get-Content $FullPath
+$content.PackageManifest.Metadata.Identity.Version = $version
+$content.Save($FullPath)
