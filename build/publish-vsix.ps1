9caa888b233fb783177002eba177ed33939765cd
Meziantou <meziantou@live.fr>
2019-05-17 18:42:04 -0400

    wip

---
+++ build/publish-vsix.ps1
@@ -0,0 +1,14 @@
+﻿$VsixPath = "src\Meziantou.Analyzer.Vsix\bin\Release\Meziantou.Analyzer.vsix"
+$ManifestPath = "src\Meziantou.Analyzer.Vsix\bin\Release\extension-manifest.json"
+$PersonalAccessToken = $env:PersonalAccessTokenMarketplace
+
+$Installation = & "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -prerelease -format json | ConvertFrom-Json
+$Path = $Installation.installationPath
+
+Write-Host $Path
+$VsixPublisher = Join-Path -Path $Path -ChildPath "VSSDK\VisualStudioIntegration\Tools\Bin\VsixPublisher.exe" -Resolve
+
+Write-Host $VsixPublisher
+Test-Path $VsixPublisher
+
+& $VsixPublisher publish -payload $VsixPath -publishManifest $ManifestPath -personalAccessToken $PersonalAccessToken -ignoreWarnings "VSIXValidatorWarning01,VSIXValidatorWarning02,VSIXValidatorWarning08"
