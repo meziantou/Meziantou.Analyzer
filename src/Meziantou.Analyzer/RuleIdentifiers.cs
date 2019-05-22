2521ce6281f80d1df06b06e8a2fce941b1198b6f
meziantou <meziantou@live.fr>
2019-05-22 19:57:29 -0400

    Add MA0054 (#34)

--- src/Meziantou.Analyzer/RuleIdentifiers.cs
+++ src/Meziantou.Analyzer/RuleIdentifiers.cs
@@ -57,6 +57,7 @@ namespace Meziantou.Analyzer
         public const string MethodShouldNotBeTooLong = "MA0051";
         public const string ReplaceEnumToStringWithNameof = "MA0052";
         public const string ClassMustBeSealed = "MA0053";
+        public const string IncludeCatchExceptionAsInnerException = "MA0054";
 
         public static string GetHelpUri(string idenfifier)
         {
---
+++ tests/Meziantou.Analyzer.Test/Rules/IncludeCatchExceptionAsInnerExceptionAnalyzerTests.cs
@@ -0,0 +1,107 @@
+﻿using System.Threading.Tasks;
+using Meziantou.Analyzer.Rules;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestHelper;
+
+namespace Meziantou.Analyzer.Test.Rules
+{
+    [TestClass]
+    public sealed class IncludeCatchExceptionAsInnerExceptionAnalyzerTests
+    {
+        private static ProjectBuilder CreateProjectBuilder()
+        {
+            return new ProjectBuilder()
+                .WithAnalyzer<IncludeCatchExceptionAsInnerExceptionAnalyzer>();
+        }
+
+        [TestMethod]
+        public async Task NotInCatchException_ShouldNotReportDiagnostic()
+        {
+            const string SourceCode = @"
+class Test
+{
+    public void A()
+    {
+        throw new System.Exception("""");
+    }
+}";
+            await CreateProjectBuilder()
+                  .WithSourceCode(SourceCode)
+                  .ValidateAsync();
+        }
+
+        [TestMethod]
+        public async Task InCatchExceptionWithInnerException_ShouldNotReportDiagnostic()
+        {
+            const string SourceCode = @"
+class Test
+{
+    public void A()
+    {
+        try
+        {
+        }
+        catch (System.Exception ex)
+        {
+            throw new System.Exception("""", ex);
+        }
+    }
+}";
+            await CreateProjectBuilder()
+                  .WithSourceCode(SourceCode)
+                  .ValidateAsync();
+        }
+
+        [TestMethod]
+        public async Task InCatchExceptionWithoutInnerException_ShouldReportDiagnostic()
+        {
+            const string SourceCode = @"
+class Test
+{
+    public void A()
+    {
+        try
+        {
+        }
+        catch (System.Exception ex)
+        {
+            throw [|]new System.Exception("""");
+        }
+    }
+}";
+            await CreateProjectBuilder()
+                  .WithSourceCode(SourceCode)
+                  .ValidateAsync();
+        }
+
+        [TestMethod]
+        public async Task InCatchExceptionWithoutInnerException_NoConstructorWithInnerException_ShouldNotReportDiagnostic()
+        {
+            const string SourceCode = @"
+class Test
+{
+    public void A()
+    {
+        try
+        {
+        }
+        catch (System.Exception ex)
+        {
+            throw new CustomException("""");
+        }
+    }
+}
+
+class CustomException : System.Exception
+{
+    public CustomException(string message)
+    {
+    }
+}
+";
+            await CreateProjectBuilder()
+                  .WithSourceCode(SourceCode)
+                  .ValidateAsync();
+        }
+    }
+}
