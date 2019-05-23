64b714861dae2805926fdc89c9d66c3d47191d35
meziantou <meziantou@live.fr>
2019-05-23 17:34:57 -0400

    Add MA0056 Do not call virtual method in constructor

---
+++ src/Meziantou.Analyzer/Rules/DoNotCallVirtualMethodInConstructorAnalyzer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace Meziantou.Analyzer.Rules
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public sealed class DoNotCallVirtualMethodInConstructorAnalyzer : DiagnosticAnalyzer
+    {
+        private static readonly DiagnosticDescriptor s_rule = new DiagnosticDescriptor(
+            RuleIdentifiers.DoNotCallVirtualMethodInConstructor,
+            title: "Do not call virtual method in constructor",
+            messageFormat: "Do not call virtual method in constructor",
+            RuleCategories.Design,
+            DiagnosticSeverity.Warning,
+            isEnabledByDefault: true,
+            description: "",
+            helpLinkUri: RuleIdentifiers.GetHelpUri(RuleIdentifiers.DoNotCallVirtualMethodInConstructor));
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(s_rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+
+            context.RegisterSyntaxNodeAction(AnalyzeConstructorSyntax, SyntaxKind.ConstructorDeclaration);
+        }
+
+        private static void AnalyzeConstructorSyntax(SyntaxNodeAnalysisContext context)
+        {
+            var node = (ConstructorDeclarationSyntax)context.Node;
+            var methodSymbol = context.SemanticModel.GetDeclaredSymbol(node, context.CancellationToken) as IMethodSymbol;
+            if (methodSymbol == null || methodSymbol.MethodKind != MethodKind.Constructor)
+                return;
+
+            // A sealed class cannot contains virtual methods
+            if (methodSymbol.ContainingType.IsSealed)
+                return;
+
+            var body = (SyntaxNode)node.Body ?? node.ExpressionBody;
+            if (body == null)
+                return;
+
+            var invocationSyntaxes = body.DescendantNodes().OfType<InvocationExpressionSyntax>();
+            foreach (var invocationSyntax in invocationSyntaxes)
+            {
+                var operation = context.SemanticModel.GetOperation(invocationSyntax, context.CancellationToken) as IInvocationOperation;
+                if (operation == null)
+                    continue;
+
+                if (operation.TargetMethod.IsVirtual && IsThis(operation))
+                {
+                    context.ReportDiagnostic(s_rule, invocationSyntax);
+                }
+            }
+        }
+
+        private static bool IsThis(IInvocationOperation operation)
+        {
+            return operation.Instance is IInstanceReferenceOperation i && i.ReferenceKind == InstanceReferenceKind.ContainingTypeInstance;
+        }
+    }
+}
---
+++ tests/Meziantou.Analyzer.Test/Rules/DoNotCallVirtualMethodInConstructorAnalyzerTests.cs
@@ -0,0 +1,72 @@
+﻿using System.Threading.Tasks;
+using Meziantou.Analyzer.Rules;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestHelper;
+
+namespace Meziantou.Analyzer.Test.Rules
+{
+    [TestClass]
+    public sealed class DoNotCallVirtualMethodInConstructorAnalyzerTests
+    {
+        private static ProjectBuilder CreateProjectBuilder()
+        {
+            return new ProjectBuilder()
+                .WithAnalyzer<DoNotCallVirtualMethodInConstructorAnalyzer>();
+        }
+
+        [TestMethod]
+        public async Task CtorWithVirtualCall()
+        {
+            const string SourceCode = @"
+class Test
+{
+    Test()
+    {
+        [|]A();
+    }
+
+    public virtual void A() { }
+}";
+            await CreateProjectBuilder()
+                  .WithSourceCode(SourceCode)
+                  .ValidateAsync();
+        }
+
+        [TestMethod]
+        public async Task CtorWithNoVirtualCall()
+        {
+            const string SourceCode = @"
+class Test
+{
+    Test()
+    {
+        A();
+    }
+
+    public void A() { }
+}";
+            await CreateProjectBuilder()
+                  .WithSourceCode(SourceCode)
+                  .ValidateAsync();
+        }
+
+        [TestMethod]
+        public async Task CtorWithVirtualCallOnAnotherInstance()
+        {
+            const string SourceCode = @"
+class Test
+{
+    Test()
+    {
+        var test = new Test();
+        test.A();
+    }
+
+    public virtual void A() { }
+}";
+            await CreateProjectBuilder()
+                  .WithSourceCode(SourceCode)
+                  .ValidateAsync();
+        }
+    }
+}
