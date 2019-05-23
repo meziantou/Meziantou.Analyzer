b7bcc14a0b308e3170f5aaf4100d5c7087ea643e
meziantou <meziantou@live.fr>
2019-05-23 17:04:54 -0400

    Add MA0055 do not use destructor (#38)

---
+++ src/Meziantou.Analyzer/Rules/DoNotUseDestructorAnalyzer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Meziantou.Analyzer.Rules
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public sealed class DoNotUseDestructorAnalyzer : DiagnosticAnalyzer
+    {
+        private static readonly DiagnosticDescriptor s_rule = new DiagnosticDescriptor(
+            RuleIdentifiers.DoNotUseDestructor,
+            title: "Do not use desctructor",
+            messageFormat: "Do not use desctructor",
+            RuleCategories.Design,
+            DiagnosticSeverity.Warning,
+            isEnabledByDefault: true,
+            description: "",
+            helpLinkUri: RuleIdentifiers.GetHelpUri(RuleIdentifiers.DoNotUseDestructor));
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(s_rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+
+            context.RegisterSymbolAction(AnalyzeMethodSymbol, SymbolKind.Method);
+        }
+
+        private void AnalyzeMethodSymbol(SymbolAnalysisContext context)
+        {
+            var symbol = (IMethodSymbol)context.Symbol;
+            if (symbol.MethodKind == MethodKind.Destructor)
+            {
+                context.ReportDiagnostic(s_rule, symbol);
+            }
+        }
+    }
+}
