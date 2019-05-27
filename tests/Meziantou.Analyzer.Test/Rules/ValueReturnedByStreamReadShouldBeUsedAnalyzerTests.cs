632276325d776c8fde759071ad807d8b97b62e82
meziantou <meziantou@live.fr>
2019-05-27 17:50:29 -0400

    Add rule MA0060 the length returned from Stream.Read should be checked (#42)

---
+++ tests/Meziantou.Analyzer.Test/Rules/ValueReturnedByStreamReadShouldBeUsedAnalyzerTests.cs
@@ -0,0 +1,89 @@
+﻿using System.Threading.Tasks;
+using Meziantou.Analyzer.Rules;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestHelper;
+
+namespace Meziantou.Analyzer.Test.Rules
+{
+    [TestClass]
+    public sealed class ValueReturnedByStreamReadShouldBeUsedAnalyzerTests
+    {
+        private static ProjectBuilder CreateProjectBuilder()
+        {
+            return new ProjectBuilder()
+                .WithAnalyzer<ValueReturnedByStreamReadShouldBeUsedAnalyzer>();
+        }
+
+        [TestMethod]
+        public async Task Read_ReturnValueNotUsed()
+        {
+            const string SourceCode = @"using System.IO;
+class Test
+{
+    void A()
+    {
+        var stream = File.OpenRead("""");
+        [|]stream.Read(null, 0, 0);
+    }
+}
+";
+            await CreateProjectBuilder()
+                  .WithSourceCode(SourceCode)
+                  .ValidateAsync();
+        }
+
+        [TestMethod]
+        public async Task ReadAsync_ReturnValueNotUsed()
+        {
+            const string SourceCode = @"using System.IO;
+class Test
+{
+    async void A()
+    {
+        var stream = File.OpenRead("""");
+        await [|]stream.ReadAsync(null, 0, 0);
+    }
+}
+";
+            await CreateProjectBuilder()
+                  .WithSourceCode(SourceCode)
+                  .ValidateAsync();
+        }
+
+        [TestMethod]
+        public async Task ReadAsync_ReturnValueUsed_DiscardOperator()
+        {
+            const string SourceCode = @"using System.IO;
+class Test
+{
+    async void A()
+    {
+        var stream = File.OpenRead("""");
+        _ = await stream.ReadAsync(null, 0, 0);
+    }
+}
+";
+            await CreateProjectBuilder()
+                  .WithSourceCode(SourceCode)
+                  .ValidateAsync();
+        }
+
+        [TestMethod]
+        public async Task Read_ReturnValueUsed_MethodCall()
+        {
+            const string SourceCode = @"using System.IO;
+class Test
+{
+    async void A()
+    {
+        var stream = File.OpenRead("""");
+        System.Console.Write(stream.Read(null, 0, 0));
+    }
+}
+";
+            await CreateProjectBuilder()
+                  .WithSourceCode(SourceCode)
+                  .ValidateAsync();
+        }
+    }
+}
