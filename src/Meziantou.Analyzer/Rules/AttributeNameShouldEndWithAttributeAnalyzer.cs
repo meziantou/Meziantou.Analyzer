3c484b642ae9f165e8aab70e3e2a4692f8fc6585
Gérald Barré <meziantou@users.noreply.github.com>
2019-05-27 17:16:52 -0400

    Add naming rules for Attribute, EventArgs, and Exception (#41)

---
+++ src/Meziantou.Analyzer/Rules/AttributeNameShouldEndWithAttributeAnalyzer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Meziantou.Analyzer.Rules
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public sealed class AttributeNameShouldEndWithAttributeAnalyzer : DiagnosticAnalyzer
+    {
+        private static readonly DiagnosticDescriptor s_rule = new DiagnosticDescriptor(
+            RuleIdentifiers.AttributeNameShouldEndWithAttribute,
+            title: "Class name should end with 'Attribute'",
+            messageFormat: "Class name should end with 'Attribute'",
+            RuleCategories.Naming,
+            DiagnosticSeverity.Info,
+            isEnabledByDefault: true,
+            description: "",
+            helpLinkUri: RuleIdentifiers.GetHelpUri(RuleIdentifiers.AttributeNameShouldEndWithAttribute));
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(s_rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+
+            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+        }
+
+        private static void AnalyzeSymbol(SymbolAnalysisContext context)
+        {
+            var symbol = (INamedTypeSymbol)context.Symbol;
+            if (symbol.Name == null)
+                return;
+
+            if (!symbol.Name.EndsWith("Attribute", System.StringComparison.Ordinal) && symbol.InheritsFrom(context.Compilation.GetTypeByMetadataName("System.Attribute")))
+            {
+                context.ReportDiagnostic(s_rule, symbol);
+            }
+        }
+    }
+}
---
+++ tests/Meziantou.Analyzer.Test/Rules/AttributeNameShouldEndWithAttributeAnalyzerTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+using Meziantou.Analyzer.Rules;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestHelper;
+
+namespace Meziantou.Analyzer.Test.Rules
+{
+    [TestClass]
+    public sealed class AttributeNameShouldEndWithAttributeAnalyzerTests
+    {
+        private static ProjectBuilder CreateProjectBuilder()
+        {
+            return new ProjectBuilder()
+                .WithAnalyzer<AttributeNameShouldEndWithAttributeAnalyzer>();
+        }
+
+        [TestMethod]
+        public async Task NameEndsWithAttribute()
+        {
+            const string SourceCode = @"
+class CustomAttribute : System.Attribute
+{
+}
+";
+            await CreateProjectBuilder()
+                  .WithSourceCode(SourceCode)
+                  .ValidateAsync();
+        }
+
+        [TestMethod]
+        public async Task NameDoesNotEndWithAttribute()
+        {
+            const string SourceCode = @"
+class [|]CustomAttr : System.Attribute
+{
+}
+";
+            await CreateProjectBuilder()
+                  .WithSourceCode(SourceCode)
+                  .ValidateAsync();
+        }
+
+    }
+}
