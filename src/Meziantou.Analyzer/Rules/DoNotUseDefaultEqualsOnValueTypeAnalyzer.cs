a45825c30ab5c1acb91a0c1265e9150e200732ed
meziantou <meziantou@live.fr>
2019-06-30 00:01:10 -0400

    Add MA0064 and MA0065

---
+++ src/Meziantou.Analyzer/Rules/DoNotUseDefaultEqualsOnValueTypeAnalyzer.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace Meziantou.Analyzer.Rules
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public sealed class DoNotUseDefaultEqualsOnValueTypeAnalyzer : DiagnosticAnalyzer
+    {
+        private static readonly DiagnosticDescriptor s_rule = new DiagnosticDescriptor(
+            RuleIdentifiers.DoNotUseDefaultEqualsOnValueType,
+            title: "Default ValueType.Equals or HashCode is used for struct's equality",
+            messageFormat: "Default ValueType.Equals or HashCode is used for struct's equality",
+            RuleCategories.Performance,
+            DiagnosticSeverity.Warning,
+            isEnabledByDefault: true,
+            description: "",
+            helpLinkUri: RuleIdentifiers.GetHelpUri(RuleIdentifiers.DoNotUseDefaultEqualsOnValueType));
+
+        private static readonly DiagnosticDescriptor s_rule2 = new DiagnosticDescriptor(
+            RuleIdentifiers.StructWithDefaultEqualsImplementationUsedAsAKey,
+            title: "Hash table unfriendly type is used in a hash table",
+            messageFormat: "Hash table unfriendly type is used in a hash table",
+            RuleCategories.Performance,
+            DiagnosticSeverity.Warning,
+            isEnabledByDefault: true,
+            description: "",
+            helpLinkUri: RuleIdentifiers.GetHelpUri(RuleIdentifiers.StructWithDefaultEqualsImplementationUsedAsAKey));
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(s_rule, s_rule2);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+
+            context.RegisterCompilationStartAction(ctx =>
+            {
+                var analyzeContext = new Context(ctx.Compilation);
+                ctx.RegisterOperationAction(analyzeContext.AnalyzeInvocationOperation, OperationKind.Invocation);
+                ctx.RegisterOperationAction(analyzeContext.AnalyzeFieldReferenceOperation, OperationKind.FieldReference);
+                ctx.RegisterOperationAction(analyzeContext.AnalyzeObjectCreationOperation, OperationKind.ObjectCreation);
+            });
+        }
+
+        private sealed class Context
+        {
+            public Compilation Compilation { get; }
+            private INamedTypeSymbol IEqualityComparerSymbol { get; }
+            private INamedTypeSymbol IComparerSymbol { get; }
+            private ITypeSymbol ValueTypeSymbol { get; }
+            private ITypeSymbol ImmutableDictionarySymbol { get; }
+            private ITypeSymbol ImmutableHashSetSymbol { get; }
+            private ITypeSymbol ImmutableSortedDictionarySymbol { get; }
+            private IMethodSymbol ValueTypeEqualsSymbol { get; }
+            private IMethodSymbol ValueTypeGetHashCodeSymbol { get; }
+            private ITypeSymbol[] HashSetSymbols { get; }
+
+            public Context(Compilation compilation)
+            {
+                IEqualityComparerSymbol = compilation.GetTypeByMetadataName("System.Collections.Generic.IEqualityComparer`1");
+                IComparerSymbol = compilation.GetTypeByMetadataName("System.Collections.Generic.IComparer`1");
+                ValueTypeSymbol = compilation.GetTypeByMetadataName("System.ValueType");
+                if (ValueTypeSymbol != null)
+                {
+                    ValueTypeEqualsSymbol = ValueTypeSymbol.GetMembers(nameof(ValueType.Equals)).OfType<IMethodSymbol>().FirstOrDefault();
+                    ValueTypeGetHashCodeSymbol = ValueTypeSymbol.GetMembers(nameof(ValueType.Equals)).OfType<IMethodSymbol>().FirstOrDefault();
+                }
+
+                ImmutableDictionarySymbol = compilation.GetTypeByMetadataName("System.Collections.Immutable.ImmutableDictionary");
+                ImmutableHashSetSymbol = compilation.GetTypeByMetadataName("System.Collections.Immutable.ImmutableHashSet");
+                ImmutableSortedDictionarySymbol = compilation.GetTypeByMetadataName("System.Collections.Immutable.ImmutableSortedDictionary");
+
+                var types = new List<ITypeSymbol>();
+                types.AddIfNotNull(compilation.GetTypeByMetadataName("System.Collections.Generic.HashSet`1"));
+                types.AddIfNotNull(compilation.GetTypeByMetadataName("System.Collections.Generic.Dictionary`2"));
+                types.AddIfNotNull(compilation.GetTypesByMetadataName("System.Collections.Concurrent.ConcurrentDictionary`2"));
+                types.AddIfNotNull(compilation.GetTypesByMetadataName("System.Collections.Immutable.ImmutableHashSet`1"));
+                types.AddIfNotNull(compilation.GetTypesByMetadataName("System.Collections.Immutable.ImmutableDictionary`2"));
+                types.AddIfNotNull(compilation.GetTypesByMetadataName("System.Collections.Immutable.ImmutableSortedDictionary`2"));
+                HashSetSymbols = types.ToArray();
+                Compilation = compilation;
+            }
+
+            public void AnalyzeInvocationOperation(OperationAnalysisContext context)
+            {
+                var operation = (IInvocationOperation)context.Operation;
+
+                if (operation.TargetMethod.Name == nameof(ValueType.GetHashCode))
+                {
+                    var actualType = operation.Children.FirstOrDefault()?.GetActualType();
+                    if (actualType == null)
+                        return;
+
+                    if (actualType.IsOrInheritFrom(ValueTypeSymbol) && HasDefaultEqualsOrHashCodeImplementations(actualType))
+                    {
+                        context.ReportDiagnostic(s_rule, operation);
+                    }
+                }
+                else if (operation.TargetMethod.Name == nameof(ValueType.Equals))
+                {
+                    var actualType = operation.Children.FirstOrDefault()?.GetActualType();
+                    if (actualType == null)
+                        return;
+
+                    if (actualType.IsOrInheritFrom(ValueTypeSymbol) && HasDefaultEqualsOrHashCodeImplementations(actualType))
+                    {
+                        context.ReportDiagnostic(s_rule, operation);
+                    }
+                }
+                else if (IsImmutableCreateMethod(operation.TargetMethod))
+                {
+                    var type = operation.TargetMethod.TypeArguments[0];
+                    if (type.IsOrInheritFrom(ValueTypeSymbol) && HasDefaultEqualsOrHashCodeImplementations(type))
+                    {
+                        if (operation.TargetMethod.ContainingType.IsEqualTo(ImmutableSortedDictionarySymbol))
+                        {
+                            if (operation.TargetMethod.Parameters.Any(arg => arg.Type.IsEqualTo(IComparerSymbol.Construct(type))))
+                                return;
+                        }
+                        else
+                        {
+                            if (operation.TargetMethod.Parameters.Any(arg => arg.Type.IsEqualTo(IEqualityComparerSymbol.Construct(type))))
+                                return;
+                        }
+
+                        context.ReportDiagnostic(s_rule2, operation);
+                    }
+                }
+
+                bool IsImmutableCreateMethod(IMethodSymbol methodSymbol)
+                {
+                    var names = new[]
+                    {
+                        "Create",
+                        "CreateBuilder",
+                        "CreateRange",
+                    };
+
+                    var builderTypes = new[]
+                    {
+                        ImmutableDictionarySymbol,
+                        ImmutableHashSetSymbol,
+                        ImmutableSortedDictionarySymbol,
+                    };
+
+                    return methodSymbol.Arity >= 1 && names.Contains(methodSymbol.Name, StringComparer.Ordinal) && builderTypes.Any(type => type.IsEqualTo(methodSymbol.ContainingType.OriginalDefinition));
+                }
+            }
+
+            public void AnalyzeFieldReferenceOperation(OperationAnalysisContext context)
+            {
+                var operation = (IFieldReferenceOperation)context.Operation;
+                if (operation.Field.Name == "Empty")
+                {
+                    var type = operation.Field.ContainingType as INamedTypeSymbol;
+                    if (type?.OriginalDefinition == null)
+                        return;
+
+                    if (HashSetSymbols.Any(t => type.OriginalDefinition.Equals(t)))
+                    {
+                        if (type.TypeArguments[0].IsOrInheritFrom(ValueTypeSymbol) && HasDefaultEqualsOrHashCodeImplementations(type.TypeArguments[0]))
+                        {
+                            context.ReportDiagnostic(s_rule2, operation);
+                        }
+                    }
+                }
+            }
+
+            public void AnalyzeObjectCreationOperation(OperationAnalysisContext context)
+            {
+                var operation = (IObjectCreationOperation)context.Operation;
+                var type = operation.Type as INamedTypeSymbol;
+                if (type?.OriginalDefinition == null)
+                    return;
+
+                if (HashSetSymbols.Any(t => type.OriginalDefinition.Equals(t)))
+                {
+                    if (operation.Constructor.Parameters.Any(arg => arg.Type.IsEqualTo(IEqualityComparerSymbol.Construct(type.TypeArguments[0]))))
+                        return;
+
+                    if (type.TypeArguments[0].IsOrInheritFrom(ValueTypeSymbol) && HasDefaultEqualsOrHashCodeImplementations(type.TypeArguments[0]))
+                    {
+                        context.ReportDiagnostic(s_rule2, operation);
+                    }
+                }
+            }
+
+            private bool HasDefaultEqualsOrHashCodeImplementations(ITypeSymbol typeSymbol)
+            {
+                if (ValueTypeEqualsSymbol != null && typeSymbol.GetMembers(ValueTypeEqualsSymbol.Name).OfType<IMethodSymbol>().FirstOrDefault(member => member.IsOverride && ValueTypeEqualsSymbol.Equals(member.OverriddenMethod)) == null)
+                    return true;
+
+                if (ValueTypeGetHashCodeSymbol != null && typeSymbol.GetMembers(ValueTypeGetHashCodeSymbol.Name).OfType<IMethodSymbol>().FirstOrDefault(member => member.IsOverride && ValueTypeGetHashCodeSymbol.Equals(member.OverriddenMethod)) == null)
+                    return true;
+
+                return false;
+            }
+        }
+    }
+}
---
+++ tests/Meziantou.Analyzer.Test/Rules/DoNotUseDefaultEqualsOnValueTypeAnalyzer_HashSetTests.cs
@@ -0,0 +1,95 @@
+﻿using System.Threading.Tasks;
+using Meziantou.Analyzer.Rules;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestHelper;
+
+namespace Meziantou.Analyzer.Test.Rules
+{
+    [TestClass]
+    public sealed class DoNotUseDefaultEqualsOnValueTypeAnalyzer_HashSetTests
+    {
+        private static ProjectBuilder CreateProjectBuilder()
+        {
+            return new ProjectBuilder()
+                .WithAnalyzer<DoNotUseDefaultEqualsOnValueTypeAnalyzer>(id: "MA0066");
+        }
+
+        [DataTestMethod]
+        [DataRow("new System.Collections.Generic.HashSet<Test>()")]
+        [DataRow("new System.Collections.Generic.Dictionary<Test, object>()")]
+        [DataRow("new System.Collections.Concurrent.ConcurrentDictionary<Test, object>()")]
+        [DataRow("System.Collections.Immutable.ImmutableHashSet.Create<Test>()")]
+        [DataRow("System.Collections.Immutable.ImmutableDictionary.Create<Test, object>()")]
+        [DataRow("System.Collections.Immutable.ImmutableSortedDictionary.Create<Test, object>()")]
+        [DataRow("System.Collections.Immutable.ImmutableHashSet<Test>.Empty")]
+        [DataRow("System.Collections.Immutable.ImmutableDictionary<Test, object>.Empty")]
+        [DataRow("System.Collections.Immutable.ImmutableSortedDictionary<Test, object>.Empty")]
+        public async Task Constructor_DefaultImplementation(string text)
+        {
+            var sourceCode = @"
+struct Test
+{
+    void A()
+    {
+        var collection = [|]" + text + @";
+    }
+}
+";
+            await CreateProjectBuilder()
+                  .WithSourceCode(sourceCode)
+                  .ValidateAsync();
+        }
+
+        [DataTestMethod]
+        [DataRow("new System.Collections.Generic.HashSet<Test>()")]
+        [DataRow("new System.Collections.Generic.Dictionary<Test, object>()")]
+        [DataRow("new System.Collections.Concurrent.ConcurrentDictionary<Test, object>()")]
+        [DataRow("System.Collections.Immutable.ImmutableHashSet.Create<Test>()")]
+        [DataRow("System.Collections.Immutable.ImmutableDictionary.Create<Test, object>()")]
+        [DataRow("System.Collections.Immutable.ImmutableSortedDictionary.Create<Test, object>()")]
+        [DataRow("System.Collections.Immutable.ImmutableHashSet<Test>.Empty")]
+        [DataRow("System.Collections.Immutable.ImmutableDictionary<Test, object>.Empty")]
+        [DataRow("System.Collections.Immutable.ImmutableSortedDictionary<Test, object>.Empty")]
+        public async Task Constructor_EqualsOverriden(string text)
+        {
+            var sourceCode = @"
+struct Test
+{
+    public override bool Equals(object o) => throw null;
+    public override int GetHashCode() => throw null;
+
+    void A()
+    {
+        _ = " + text + @";
+    }
+}
+";
+            await CreateProjectBuilder()
+                  .WithSourceCode(sourceCode)
+                  .ValidateAsync();
+        }
+
+        [DataTestMethod]
+        [DataRow("new System.Collections.Generic.HashSet<Test>(System.Collections.Generic.EqualityComparer<Test>.Default)")]
+        [DataRow("new System.Collections.Generic.Dictionary<Test, object>(System.Collections.Generic.EqualityComparer<Test>.Default)")]
+        [DataRow("new System.Collections.Concurrent.ConcurrentDictionary<Test, object>(System.Collections.Generic.EqualityComparer<Test>.Default)")]
+        [DataRow("System.Collections.Immutable.ImmutableHashSet.Create<Test>(System.Collections.Generic.EqualityComparer<Test>.Default)")]
+        [DataRow("System.Collections.Immutable.ImmutableDictionary.Create<Test, object>(System.Collections.Generic.EqualityComparer<Test>.Default)")]
+        [DataRow("System.Collections.Immutable.ImmutableSortedDictionary.Create<Test, object>(null, System.Collections.Generic.EqualityComparer<object>.Default)")]
+        public async Task Constructor_EqualityComparer(string text)
+        {
+            var sourceCode = @"
+struct Test
+{
+    void A()
+    {
+        _ = " + text + @";
+    }
+}
+";
+            await CreateProjectBuilder()
+                  .WithSourceCode(sourceCode)
+                  .ValidateAsync();
+        }
+    }
+}
