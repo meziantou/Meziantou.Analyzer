d7052b57fb859c258a500473117e9f9abded7b27
Gérald Barré <gerald.barre@ubisoft.com>
2019-12-04 17:32:18 -0500

    Add new rule: Use Guid.Empty instead of new Guid()

---
+++ src/Meziantou.Analyzer/Rules/UseGuidEmptyAnalyzer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace Meziantou.Analyzer.Rules
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public sealed class UseGuidEmptyAnalyzer : DiagnosticAnalyzer
+    {
+        private static readonly DiagnosticDescriptor s_rule = new DiagnosticDescriptor(
+            RuleIdentifiers.UseGuidEmpty,
+            title: "Use Guid.Empty",
+            messageFormat: "Use Guid.Empty",
+            RuleCategories.Design,
+            DiagnosticSeverity.Info,
+            isEnabledByDefault: true,
+            description: "",
+            helpLinkUri: RuleIdentifiers.GetHelpUri(RuleIdentifiers.UseGuidEmpty));
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(s_rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+
+            context.RegisterCompilationStartAction(compilationContext =>
+            {
+                compilationContext.RegisterOperationAction(AnalyzeObjectCreationOperation, OperationKind.ObjectCreation);
+            });
+        }
+
+        private static void AnalyzeObjectCreationOperation(OperationAnalysisContext context)
+        {
+            var operation = (IObjectCreationOperation)context.Operation;
+            var guidType = context.Compilation.GetTypeByMetadataName("System.Guid");
+            if (operation.Type.IsEqualTo(guidType) && operation.Arguments.Length == 0)
+            {
+                context.ReportDiagnostic(s_rule, operation);
+            }
+        }
+    }
+}
