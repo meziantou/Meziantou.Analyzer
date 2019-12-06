bbbef4365490ea00672d2635e417e28704c7e0a7
meziantou <meziantou@live.fr>
2019-12-06 16:43:29 -0500

    New rule Non-constant static fields should not be visible

---
+++ src/Meziantou.Analyzer/Rules/NonConstantStaticFieldsShouldNotBeVisibleAnalyzer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Meziantou.Analyzer.Rules
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public sealed class NonConstantStaticFieldsShouldNotBeVisibleAnalyzer : DiagnosticAnalyzer
+    {
+        private static readonly DiagnosticDescriptor s_rule = new DiagnosticDescriptor(
+            RuleIdentifiers.NonConstantStaticFieldsShouldNotBeVisible,
+            title: "Non-constant static fields should not be visible",
+            messageFormat: "Non-constant static fields should not be visible",
+            RuleCategories.Design,
+            DiagnosticSeverity.Warning,
+            isEnabledByDefault: true,
+            description: "",
+            helpLinkUri: RuleIdentifiers.GetHelpUri(RuleIdentifiers.NonConstantStaticFieldsShouldNotBeVisible));
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(s_rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+
+            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.Field);
+        }
+
+        private void AnalyzeSymbol(SymbolAnalysisContext context)
+        {
+            var symbol = (IFieldSymbol)context.Symbol;
+            if (!symbol.IsStatic || symbol.IsReadOnly || !symbol.IsVisibleOutsideOfAssembly())
+                return;
+
+            context.ReportDiagnostic(s_rule, symbol);
+        }
+    }
+}
