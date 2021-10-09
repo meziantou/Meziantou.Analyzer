c2fa4dd434f47ae638901970a58ce7892f25c887
Gérald Barré <meziantou@live.fr>
2021-10-08 20:03:19 -0400

    test

--- src/ListDotNetTypes/Program.cs
+++ src/ListDotNetTypes/Program.cs
@@ -2,47 +2,62 @@
 using System.Reflection;
 using System.Reflection.Metadata;
 using System.Reflection.PortableExecutable;
+using NuGet.Common;
+using NuGet.Protocol;
+using NuGet.Protocol.Core.Types;
 
-var packages = new (string PackageName, string PackageVersion)[]
+var packages = new[]
 {
-    ("Microsoft.NETCore.App.Ref", "6.0.0-rc.1.21451.13"),       // https://www.nuget.org/packages/Microsoft.NETCore.App.Ref
-    ("Microsoft.AspNetCore.App.Ref", "6.0.0-rc.1.21452.15"),    // https://www.nuget.org/packages/Microsoft.AspNetCore.App.Ref
-    ("Microsoft.WindowsDesktop.App.Ref", "6.0.0-rc.1.21451.3"), // https://www.nuget.org/packages/Microsoft.WindowsDesktop.App.Ref
+    "Microsoft.NETCore.App.Ref",        // https://www.nuget.org/packages/Microsoft.NETCore.App.Ref
+    "Microsoft.AspNetCore.App.Ref",     // https://www.nuget.org/packages/Microsoft.AspNetCore.App.Ref
+    "Microsoft.WindowsDesktop.App.Ref", // https://www.nuget.org/packages/Microsoft.WindowsDesktop.App.Ref
 };
 
-using var httpClient = new HttpClient();
-var types = new HashSet<string>();
-foreach (var (packageName, packageVersion) in packages)
+var cache = new SourceCacheContext();
+var repository = Repository.Factory.GetCoreV3("https://api.nuget.org/v3/index.json");
+var resource = await repository.GetResourceAsync<FindPackageByIdResource>();
+
+foreach (var includePreview in new[] { false, true })
 {
-    using var nugetStream = await httpClient.GetStreamAsync($"https://www.nuget.org/api/v2/package/{packageName}/{packageVersion}");
-    using var zipArchive = new ZipArchive(nugetStream, ZipArchiveMode.Read);
-
-    foreach (var entry in zipArchive.Entries)
+    var types = new HashSet<string>();
+    foreach (var packageName in packages)
     {
-        if (!string.Equals(Path.GetExtension(entry.Name), ".dll", StringComparison.OrdinalIgnoreCase))
-            continue;
+        var versions = await resource.GetAllVersionsAsync(packageName, cache, NullLogger.Instance, CancellationToken.None);
+        var latestVersion = versions.Where(v => includePreview || !v.IsPrerelease).Max();
+        Console.WriteLine(packageName + "@" + latestVersion);
 
-        using var entryStream = CopyToMemoryStream(zipArchive.GetEntry(entry.FullName)!);
-        using var portableExecutableReader = new PEReader(entryStream);
-        var metadataReader = portableExecutableReader.GetMetadataReader();
+        using var packageStream = new MemoryStream();
+        await resource.CopyNupkgToStreamAsync(packageName, latestVersion, packageStream, cache, NullLogger.Instance, CancellationToken.None);
+        packageStream.Seek(0, SeekOrigin.Begin);
 
-        foreach (var typeDefHandle in metadataReader.TypeDefinitions)
+        using var zipArchive = new ZipArchive(packageStream, ZipArchiveMode.Read);
+        foreach (var entry in zipArchive.Entries)
         {
-            var typeDef = metadataReader.GetTypeDefinition(typeDefHandle);
-            var ns = metadataReader.GetString(typeDef.Namespace);
-            var typeName = metadataReader.GetString(typeDef.Name);
-            if (string.IsNullOrEmpty(ns))
+            if (!string.Equals(Path.GetExtension(entry.Name), ".dll", StringComparison.OrdinalIgnoreCase))
                 continue;
 
-            if (!typeDef.Attributes.HasFlag(TypeAttributes.Public))
-                continue;
+            using var entryStream = CopyToMemoryStream(zipArchive.GetEntry(entry.FullName)!);
+            using var portableExecutableReader = new PEReader(entryStream);
+            var metadataReader = portableExecutableReader.GetMetadataReader();
 
-            types.Add(ns + "." + typeName);
+            foreach (var typeDefHandle in metadataReader.TypeDefinitions)
+            {
+                var typeDef = metadataReader.GetTypeDefinition(typeDefHandle);
+                var ns = metadataReader.GetString(typeDef.Namespace);
+                var typeName = metadataReader.GetString(typeDef.Name);
+                if (string.IsNullOrEmpty(ns))
+                    continue;
+
+                if (!typeDef.Attributes.HasFlag(TypeAttributes.Public))
+                    continue;
+
+                types.Add(ns + "." + typeName);
+            }
         }
     }
-}
 
-File.WriteAllLines(args.Length > 0 ? args[0] : "../../../../Meziantou.Analyzer/Resources/bcl.txt", types.OrderBy(t => t));
+    File.WriteAllLines(Path.Combine(args.Length > 0 ? args[0] : "../../../../Meziantou.Analyzer/Resources/", includePreview ? "bcl-preview.txt" : "bcl.txt"), types.OrderBy(t => t));
+}
 
 static MemoryStream CopyToMemoryStream(ZipArchiveEntry entry)
 {
