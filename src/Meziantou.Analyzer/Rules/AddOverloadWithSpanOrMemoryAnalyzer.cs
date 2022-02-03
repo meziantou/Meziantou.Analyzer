5f35799abdfcf8258f8ffc9386006992e2ced544
Gérald Barré <meziantou@live.fr>
2022-02-02 23:24:19 -0500

    New rule MA0109: Add an overload with Span or Memory

---
+++ src/Meziantou.Analyzer/Rules/AddOverloadWithSpanOrMemoryAnalyzer.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Meziantou.Analyzer.Rules;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public sealed class AddOverloadWithSpanOrMemoryAnalyzer : DiagnosticAnalyzer
+{
+    private static readonly DiagnosticDescriptor s_rule = new(
+        RuleIdentifiers.AddOverloadWithSpanOrMemory,
+        title: "Consider adding an overload with a Span<T> or Memory<T>",
+        messageFormat: "Consider adding an overload with a Span<T> or Memory<T>",
+        RuleCategories.Design,
+        DiagnosticSeverity.Info,
+        isEnabledByDefault: false,
+        description: "",
+        helpLinkUri: RuleIdentifiers.GetHelpUri(RuleIdentifiers.AddOverloadWithSpanOrMemory));
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(s_rule);
+
+    public override void Initialize(AnalysisContext context)
+    {
+        context.EnableConcurrentExecution();
+        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+
+        context.RegisterSymbolAction(AnalyzeMethod, SymbolKind.Method);
+    }
+
+    private void AnalyzeMethod(SymbolAnalysisContext context)
+    {
+        var method = (IMethodSymbol)context.Symbol;
+        if (method.IsImplicitlyDeclared || method.IsOverrideOrInterfaceImplementation())
+            return;
+
+        if (!method.IsVisibleOutsideOfAssembly())
+            return;
+
+        if (method.MethodKind is not MethodKind.Ordinary and not MethodKind.Constructor)
+            return;
+
+        if (!method.Parameters.Any(p => p.Type.TypeKind == TypeKind.Array))
+            return;
+
+        var overloads = method.ContainingType.GetMembers(method.Name);
+        foreach (var overload in overloads.OfType<IMethodSymbol>())
+        {
+            if (IsValidOverload(context.Compilation, method, overload))
+                return;
+        }
+
+        context.ReportDiagnostic(s_rule, method);
+    }
+
+    private static bool IsValidOverload(Compilation compilation, IMethodSymbol method, IMethodSymbol overload)
+    {
+        if (overload.IsEqualTo(method))
+            return false;
+
+        if (overload.Parameters.Length != method.Parameters.Length)
+            return false;
+
+        for (var i = 0; i < method.Parameters.Length; i++)
+        {
+            var methodParameter = method.Parameters[i].Type;
+            var overloadParameter = overload.Parameters[i].Type;
+
+            var methodParameterIsArray = methodParameter.TypeKind == TypeKind.Array;
+            if (methodParameterIsArray)
+            {
+                if (!IsSpanOrMemory(compilation, overloadParameter, ((IArrayTypeSymbol)methodParameter).ElementType))
+                    return false;
+            }
+            else
+            {
+                if (!methodParameter.IsEqualTo(overloadParameter))
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static bool IsSpanOrMemory(Compilation compilation, ITypeSymbol typeSymbol, ITypeSymbol expectedArrayType)
+    {
+        var types = new string[] { "System.Span`1", "System.ReadOnlySpan`1", "System.Memory`1", "System.ReadOnlyMemory`1" };
+        foreach (var type in types)
+        {
+            if (typeSymbol.IsEqualTo(compilation.GetTypeByMetadataName(type)?.Construct(expectedArrayType)))
+                return true;
+        }
+
+        return false;
+    }
+}
---
+++ tests/Meziantou.Analyzer.Test/Rules/AddOverloadWithSpanOrMemoryAnalyzerTests.cs
@@ -0,0 +1,87 @@
+﻿using System.Threading.Tasks;
+using Meziantou.Analyzer.Rules;
+using TestHelper;
+using Xunit;
+
+namespace Meziantou.Analyzer.Test.Rules;
+
+public sealed class AddOverloadWithSpanOrMemoryAnalyzerTests
+{
+    private static ProjectBuilder CreateProjectBuilder()
+    {
+        return new ProjectBuilder()
+            .WithTargetFramework(TargetFramework.Net6_0)
+            .WithAnalyzer<AddOverloadWithSpanOrMemoryAnalyzer>();
+    }
+
+    [Fact]
+    public async Task StringArrayWithoutSpanOverload()
+    {
+        const string SourceCode = @"
+public class Test
+{
+    public void [||]A(string[] a)
+    {
+    }
+}
+";
+        await CreateProjectBuilder()
+              .WithSourceCode(SourceCode)
+              .ValidateAsync();
+    }
+    
+    [Fact]
+    public async Task StringArrayWithoutSpanOverload_Complex()
+    {
+        const string SourceCode = @"
+public class Test
+{
+    public void [||]A(string[] a, int b) { }
+    public void A(System.ReadOnlySpan<string> a, string b) { } // Not same type for b
+    public void A(System.ReadOnlySpan<string> a, int b, int c) { } // not same number of parameters
+    public void A(System.ReadOnlySpan<string> a, System.ReadOnlySpan<int> b) { } // Not same type for b
+    public void B(System.ReadOnlySpan<string> a, int b) { } // Not same method name
+}
+";
+        await CreateProjectBuilder()
+              .WithSourceCode(SourceCode)
+              .ValidateAsync();
+    }
+
+    [Theory]
+    [InlineData("System.Span<string>")]
+    [InlineData("System.ReadOnlySpan<string>")]
+    [InlineData("System.Memory<string>")]
+    [InlineData("System.ReadOnlyMemory<string>")]
+    public async Task StringArrayWithSpanOverload(string overloadType)
+    {
+        var sourceCode = @"
+public class Test
+{
+    public void A(string[] a) { }
+    public void A(" + overloadType + @" a) { }
+}
+";
+        await CreateProjectBuilder()
+              .WithSourceCode(sourceCode)
+              .ValidateAsync();
+    }
+
+    [Theory]
+    [InlineData("System.Span<string>")]
+    [InlineData("System.ReadOnlySpan<string>")]
+    [InlineData("System.Memory<string>")]
+    [InlineData("System.ReadOnlyMemory<string>")]
+    public async Task SpanWithoutOverload(string overloadType)
+    {
+        var sourceCode = @"
+public class Test
+{
+    public void A(" + overloadType + @" a) { }
+}
+";
+        await CreateProjectBuilder()
+              .WithSourceCode(sourceCode)
+              .ValidateAsync();
+    }
+}
