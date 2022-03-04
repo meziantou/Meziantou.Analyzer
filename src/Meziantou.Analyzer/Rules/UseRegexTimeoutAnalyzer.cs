4b3e38186846b34eb35cdd81da34f9588b4375a7
Gérald Barré <meziantou@live.fr>
2022-03-04 15:21:15 -0500

    MA0009 and MA0023 support RegexGeneratorAttribute

--- src/Meziantou.Analyzer/Rules/UseRegexTimeoutAnalyzer.cs
+++ src/Meziantou.Analyzer/Rules/UseRegexTimeoutAnalyzer.cs
@@ -42,6 +42,56 @@ public sealed class RegexUsageAnalyzer : DiagnosticAnalyzer
 
         context.RegisterOperationAction(AnalyzeObjectCreation, OperationKind.ObjectCreation);
         context.RegisterOperationAction(AnalyzeInvocation, OperationKind.Invocation);
+        context.RegisterSymbolAction(AnalyzeMethod, SymbolKind.Method);
+    }
+
+    private void AnalyzeMethod(SymbolAnalysisContext context)
+    {
+        var method = (IMethodSymbol)context.Symbol;
+        if (method.MethodKind is not MethodKind.Ordinary)
+            return;
+
+        var generatorAttributeSymbol = context.Compilation.GetTypeByMetadataName("System.Text.RegularExpressions.RegexGeneratorAttribute");
+        if (generatorAttributeSymbol == null)
+            return;
+
+        foreach (var attribute in method.GetAttributes())
+        {
+            // https://github.com/dotnet/runtime/issues/58880
+            if (attribute.AttributeClass.IsEqualTo(generatorAttributeSymbol))
+            {
+                // Timeout
+                if (attribute.ConstructorArguments.Length < 3)
+                {
+                    if (attribute.ApplicationSyntaxReference != null)
+                    {
+                        context.ReportDiagnostic(s_timeoutRule, attribute.ApplicationSyntaxReference);
+                    }
+                    else
+                    {
+                        context.ReportDiagnostic(s_timeoutRule, method);
+                    }
+                }
+
+                // RegexOptions.ExplicitCapture
+                if (attribute.ConstructorArguments.Length >= 2)
+                {
+                    var pattern = attribute.ConstructorArguments[0].Value as string;
+                    var regexOptions = (RegexOptions)(int)attribute.ConstructorArguments[1].Value!;
+                    if (pattern != null && ShouldAddExplicitCapture(pattern, regexOptions))
+                    {
+                        if (attribute.ApplicationSyntaxReference != null)
+                        {
+                            context.ReportDiagnostic(s_explicitCaptureRule, attribute.ApplicationSyntaxReference);
+                        }
+                        else
+                        {
+                            context.ReportDiagnostic(s_explicitCaptureRule, method);
+                        }
+                    }
+                }
+            }
+        }
     }
 
     private static void AnalyzeInvocation(OperationAnalysisContext context)
@@ -97,49 +147,68 @@ public sealed class RegexUsageAnalyzer : DiagnosticAnalyzer
 
     private static void CheckRegexOptionsArgument(OperationAnalysisContext context, int patternArgumentIndex, ImmutableArray<IArgumentOperation> arguments, ITypeSymbol? regexOptionsSymbol)
     {
-        if (regexOptionsSymbol == null)
-            return;
 
-        var arg = arguments.FirstOrDefault(a => a.Parameter != null && a.Parameter.Type.IsEqualTo(regexOptionsSymbol));
-        if (arg == null || arg.Value == null)
-            return;
-
-        if (arg.Value.ConstantValue.HasValue)
+        var pattern = GetPattern();
+        var (regexOptions, regexOptionsArgument) = GetRegexOptions();
+        if (pattern != null && regexOptions != null && regexOptionsArgument != null)
         {
-            var value = ((RegexOptions)arg.Value.ConstantValue.Value!);
-            if (!value.HasFlag(RegexOptions.ExplicitCapture) && !value.HasFlag(RegexOptions.ECMAScript)) // The 2 options are exclusives
+            if (ShouldAddExplicitCapture(pattern, regexOptions.Value))
             {
-                if (HasUnnamedGroups(value))
-                {
-                    context.ReportDiagnostic(s_explicitCaptureRule, arg);
-                }
+                context.ReportDiagnostic(s_explicitCaptureRule, regexOptionsArgument);
             }
         }
 
-        bool HasUnnamedGroups(RegexOptions options)
+        string? GetPattern()
         {
             if (patternArgumentIndex < arguments.Length)
             {
                 var argument = arguments[patternArgumentIndex];
                 if (argument.Value != null && argument.Value.ConstantValue.HasValue && argument.Value.ConstantValue.Value is string pattern)
-                {
-                    try
-                    {
-                        var regex1 = new Regex(pattern, options, Regex.InfiniteMatchTimeout);
-                        var regex2 = new Regex(pattern, options | RegexOptions.ExplicitCapture, Regex.InfiniteMatchTimeout);
+                    return pattern;
+            }
 
-                        // All groups are named => No need for explicit capture
-                        if (regex1.GetGroupNames().Length == regex2.GetGroupNames().Length)
-                            return false;
-                    }
-                    catch
-                    {
-                    }
-                }
+            return null;
+        }
+
+        (RegexOptions?, IArgumentOperation?) GetRegexOptions()
+        {
+            if (regexOptionsSymbol == null)
+                return (null, null);
+
+
+            var arg = arguments.FirstOrDefault(a => a.Parameter != null && a.Parameter.Type.IsEqualTo(regexOptionsSymbol));
+            if (arg == null || arg.Value == null || !arg.Value.ConstantValue.HasValue)
+                return (null, arg);
+
+            return ((RegexOptions)arg.Value.ConstantValue.Value!, arg);
+        }
+    }
+
+    private static bool ShouldAddExplicitCapture(string pattern, RegexOptions regexOptions)
+    {
+        if (!regexOptions.HasFlag(RegexOptions.ExplicitCapture) && !regexOptions.HasFlag(RegexOptions.ECMAScript)) // The 2 options are exclusives
+        {
+            return HasUnnamedGroups(pattern, regexOptions);
+        }
+
+        return false;
+
+        static bool HasUnnamedGroups(string pattern, RegexOptions options)
+        {
+            try
+            {
+                var regex1 = new Regex(pattern, options, Regex.InfiniteMatchTimeout);
+                var regex2 = new Regex(pattern, options | RegexOptions.ExplicitCapture, Regex.InfiniteMatchTimeout);
+
+                // All groups are named => No need for explicit capture
+                if (regex1.GetGroupNames().Length == regex2.GetGroupNames().Length)
+                    return false;
+            }
+            catch
+            {
             }
 
             return true;
         }
     }
-
 }
