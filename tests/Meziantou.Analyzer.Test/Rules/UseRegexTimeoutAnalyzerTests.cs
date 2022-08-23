93df2d9ff1d839041fa504b61dce04b3dba5e7bf
Gérald Barré <git@meziantou.net>
2022-08-22 21:58:57 -0400

    Do not report MA0009 when using RegexOptions.NonBacktracking

--- tests/Meziantou.Analyzer.Test/Rules/UseRegexTimeoutAnalyzerTests.cs
+++ tests/Meziantou.Analyzer.Test/Rules/UseRegexTimeoutAnalyzerTests.cs
@@ -44,6 +44,23 @@ class TestClass
               .WithSourceCode(SourceCode)
               .ValidateAsync();
     }
+    
+    [Fact]
+    public async Task IsMatch_NonBacktracking_WithoutTimeout_ShouldNotReportError()
+    {
+        const string SourceCode = @"using System.Text.RegularExpressions;
+class TestClass
+{
+    void Test()
+    {
+        Regex.IsMatch(""test"", ""[a-z]+"", RegexOptions.ExplicitCapture | RegexOptions.NonBacktracking);
+    }
+}";
+        await CreateProjectBuilder()
+              .WithTargetFramework(TargetFramework.Net7_0)
+              .WithSourceCode(SourceCode)
+              .ValidateAsync();
+    }
 
     [Fact]
     public async Task Ctor_MissingTimeout_ShouldReportError()
@@ -76,6 +93,23 @@ class TestClass
               .WithSourceCode(SourceCode)
               .ValidateAsync();
     }
+    
+    [Fact]
+    public async Task Ctor_WithoutTimeout_NonBacktracking_ShouldNotReportError()
+    {
+        const string SourceCode = @"using System.Text.RegularExpressions;
+class TestClass
+{
+    void Test()
+    {
+        new Regex(""[a-z]+"", RegexOptions.ExplicitCapture | RegexOptions.NonBacktracking);
+    }
+}";
+        await CreateProjectBuilder()
+              .WithSourceCode(SourceCode)
+              .WithTargetFramework(TargetFramework.Net7_0)
+              .ValidateAsync();
+    }
 
     [Fact]
     public async Task NonRegexCtor_ShouldNotReportError()
@@ -91,5 +125,25 @@ class TestClass
         await CreateProjectBuilder()
               .WithSourceCode(SourceCode)
               .ValidateAsync();
+    }
+
+    [Fact]
+    public async Task RegexGenerator_WithoutTimeout_NonBacktracking()
+    {
+        var project = CreateProjectBuilder()
+              .WithLanguageVersion(Microsoft.CodeAnalysis.CSharp.LanguageVersion.Preview)
+              .WithTargetFramework(TargetFramework.Net7_0)
+              .WithSourceCode(@"using System.Text.RegularExpressions;
+partial class TestClass
+{
+    [RegexGenerator(""pattern"", RegexOptions.NonBacktracking)]
+    private static partial Regex Test();
+}
+partial class TestClass
+{
+    private static partial Regex Test() => throw null;
+}");
+
+        await project.ValidateAsync();
     }
 }
