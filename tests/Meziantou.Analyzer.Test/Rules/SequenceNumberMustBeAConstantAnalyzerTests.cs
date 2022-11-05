c246de7fb12974bd2f6bfc788e99a19186c23558
Gérald Barré <git@meziantou.net>
2022-11-05 14:18:42 -0400

    Report on the argument

--- tests/Meziantou.Analyzer.Test/Rules/SequenceNumberMustBeAConstantAnalyzerTests.cs
+++ tests/Meziantou.Analyzer.Test/Rules/SequenceNumberMustBeAConstantAnalyzerTests.cs
@@ -62,32 +62,32 @@ class Test
     }
 
     [Theory]
-    [InlineData("builder.AddAttribute(value++, frame: default)")]
-    [InlineData("builder.AddAttribute(value++, name: default(string))")]
-    [InlineData("builder.AddAttribute(value++, name: default(string), value: default(Microsoft.AspNetCore.Components.EventCallback))")]
-    [InlineData("builder.AddAttribute(value++, name: default(string), value: false)")]
-    [InlineData("builder.AddAttribute(value++, name: default(string), value: default(MulticastDelegate))")]
-    [InlineData("builder.AddAttribute(value++, name: default(string), value: default(object))")]
-    [InlineData("builder.AddAttribute(value++, name: default(string), value: default(string))")]
-    [InlineData("builder.AddAttribute<int>(value++, name: default(string), value: default(Microsoft.AspNetCore.Components.EventCallback<int>))")]
-    [InlineData("builder.AddComponentReferenceCapture(value++, componentReferenceCaptureAction: null)")]
-    [InlineData("builder.AddContent(value++, markupContent: default(Microsoft.AspNetCore.Components.MarkupString))")]
-    [InlineData("builder.AddContent(value++, markupContent: default(Microsoft.AspNetCore.Components.MarkupString?))")]
-    [InlineData("builder.AddContent(value++, fragment: default(Microsoft.AspNetCore.Components.RenderFragment))")]
-    [InlineData("builder.AddContent<int>(value++, fragment: default(Microsoft.AspNetCore.Components.RenderFragment<int>), value: 0)")]
-    [InlineData("builder.AddContent(value++, textContent: default(object))")]
-    [InlineData("builder.AddContent(value++, textContent: default(string))")]
-    [InlineData("builder.AddElementReferenceCapture(value++, elementReferenceCaptureAction: default(Action<Microsoft.AspNetCore.Components.ElementReference>))")]
-    [InlineData("builder.AddMarkupContent(value++, markupContent: default(string))")]
-    [InlineData("builder.AddMultipleAttributes(value++, attributes: default(System.Collections.Generic.IEnumerable<System.Collections.Generic.KeyValuePair<string,object>>))")]
-    [InlineData("builder.OpenComponent(value++, componentType: default(Type))")]
-    [InlineData("builder.OpenComponent<Microsoft.AspNetCore.Components.IComponent>(value++)")]
-    [InlineData("builder.OpenElement(value++, elementName: default(string))")]
-    [InlineData("builder.OpenRegion(value++)")]
-    [InlineData("builder.AddEventPreventDefaultAttribute(value++, eventName: default(string), value: false)")]
-    [InlineData("builder.AddEventStopPropagationAttribute(value++, eventName: default(string), value: false)")]
-    [InlineData("builder.AddEventStopPropagationAttribute(param++, eventName: default(string), value: false)")]
-    [InlineData("builder.AddEventStopPropagationAttribute((int)longparam++, eventName: default(string), value: false)")]
+    [InlineData("builder.AddAttribute([|value++|], frame: default)")]
+    [InlineData("builder.AddAttribute([|value++|], name: default(string))")]
+    [InlineData("builder.AddAttribute([|value++|], name: default(string), value: default(Microsoft.AspNetCore.Components.EventCallback))")]
+    [InlineData("builder.AddAttribute([|value++|], name: default(string), value: false)")]
+    [InlineData("builder.AddAttribute([|value++|], name: default(string), value: default(MulticastDelegate))")]
+    [InlineData("builder.AddAttribute([|value++|], name: default(string), value: default(object))")]
+    [InlineData("builder.AddAttribute([|value++|], name: default(string), value: default(string))")]
+    [InlineData("builder.AddAttribute<int>([|value++|], name: default(string), value: default(Microsoft.AspNetCore.Components.EventCallback<int>))")]
+    [InlineData("builder.AddComponentReferenceCapture([|value++|], componentReferenceCaptureAction: null)")]
+    [InlineData("builder.AddContent([|value++|], markupContent: default(Microsoft.AspNetCore.Components.MarkupString))")]
+    [InlineData("builder.AddContent([|value++|], markupContent: default(Microsoft.AspNetCore.Components.MarkupString?))")]
+    [InlineData("builder.AddContent([|value++|], fragment: default(Microsoft.AspNetCore.Components.RenderFragment))")]
+    [InlineData("builder.AddContent<int>([|value++|], fragment: default(Microsoft.AspNetCore.Components.RenderFragment<int>), value: 0)")]
+    [InlineData("builder.AddContent([|value++|], textContent: default(object))")]
+    [InlineData("builder.AddContent([|value++|], textContent: default(string))")]
+    [InlineData("builder.AddElementReferenceCapture([|value++|], elementReferenceCaptureAction: default(Action<Microsoft.AspNetCore.Components.ElementReference>))")]
+    [InlineData("builder.AddMarkupContent([|value++|], markupContent: default(string))")]
+    [InlineData("builder.AddMultipleAttributes([|value++|], attributes: default(System.Collections.Generic.IEnumerable<System.Collections.Generic.KeyValuePair<string,object>>))")]
+    [InlineData("builder.OpenComponent([|value++|], componentType: default(Type))")]
+    [InlineData("builder.OpenComponent<Microsoft.AspNetCore.Components.IComponent>([|value++|])")]
+    [InlineData("builder.OpenElement([|value++|], elementName: default(string))")]
+    [InlineData("builder.OpenRegion([|value++|])")]
+    [InlineData("builder.AddEventPreventDefaultAttribute([|value++|], eventName: default(string), value: false)")]
+    [InlineData("builder.AddEventStopPropagationAttribute([|value++|], eventName: default(string), value: false)")]
+    [InlineData("builder.AddEventStopPropagationAttribute([|param++|], eventName: default(string), value: false)")]
+    [InlineData("builder.AddEventStopPropagationAttribute([|(int)longparam++|], eventName: default(string), value: false)")]
     public async Task Variable(string code)
     {
         var project = CreateProjectBuilder()
@@ -100,7 +100,7 @@ class Test
     void BuildRenderTree(RenderTreeBuilder builder, int param, long longparam)
 	{
         int value = 0;
-        [||]{{code}};
+        {{code}};
 	}
 }
 """);
