e679d8471f5b9da9b7e5671519a66767ed16bd4c
Gérald Barré <meziantou@users.noreply.github.com>
2023-04-24 21:49:44 -0400

    New rule: MA0132 do not use implicit conversion from DateTime to DateTimeOffset (#505)

---
+++ src/Meziantou.Analyzer/Rules/DoNotImplicitlyConvertDateTimeToDateTimeOffsetAnalyzer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace Meziantou.Analyzer.Rules;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public sealed class DoNotImplicitlyConvertDateTimeToDateTimeOffsetAnalyzer : DiagnosticAnalyzer
+{
+    private static readonly DiagnosticDescriptor s_rule = new(
+        RuleIdentifiers.DoNotImplicitlyConvertDateTimeToDateTimeOffset,
+        title: "Do not convert implicitly to DateTimeOffset",
+        messageFormat: "Do not convert implicitly to DateTimeOffset",
+        RuleCategories.Design,
+        DiagnosticSeverity.Warning,
+        isEnabledByDefault: true,
+        description: "",
+        helpLinkUri: RuleIdentifiers.GetHelpUri(RuleIdentifiers.DoNotImplicitlyConvertDateTimeToDateTimeOffset));
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(s_rule);
+
+    public override void Initialize(AnalysisContext context)
+    {
+        context.EnableConcurrentExecution();
+        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+
+        context.RegisterCompilationStartAction(context =>
+        {
+            var datetimeOffsetSymbol = context.Compilation.GetBestTypeByMetadataName("System.DateTimeOffset");
+            if (datetimeOffsetSymbol == null)
+                return;
+
+            context.RegisterOperationAction(context => AnalyzeConversion(context, datetimeOffsetSymbol), OperationKind.Conversion);
+        });
+    }
+
+    private static void AnalyzeConversion(OperationAnalysisContext context, INamedTypeSymbol dateTimeOffsetSymbol)
+    {
+        var operation = (IConversionOperation)context.Operation;
+        if (!operation.Conversion.IsImplicit)
+            return;
+
+        if (operation.Type.IsEqualTo(dateTimeOffsetSymbol) && operation.Operand.Type.IsDateTime())
+        {
+            // DateTime.Now and DateTime.UtcNow set the DateTime.Kind, so the conversion result is well-known
+            if (operation.Operand is IMemberReferenceOperation { Member.Name: "UtcNow" or "Now", Member.ContainingType.SpecialType: SpecialType.System_DateTime })
+                return;
+
+            context.ReportDiagnostic(s_rule, operation);
+        }
+    }
+}
---
+++ tests/Meziantou.Analyzer.Test/Rules/DoNotCompareDateTimeWithDateTimeOffsetAnalyzerTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading.Tasks;
+using Meziantou.Analyzer.Rules;
+using Microsoft.CodeAnalysis;
+using TestHelper;
+using Xunit;
+
+namespace Meziantou.Analyzer.Test.Rules;
+public class DoNotCompareDateTimeWithDateTimeOffsetAnalyzerTests
+{
+    private static ProjectBuilder CreateProjectBuilder()
+    {
+        return new ProjectBuilder()
+            .WithOutputKind(OutputKind.ConsoleApplication)
+            .WithAnalyzer<DoNotImplicitlyConvertDateTimeToDateTimeOffsetAnalyzer>();
+    }
+
+    [Fact]
+    public async Task ImplicitConversion_BinaryOperation_Subtract_UtcNow()
+    {
+        await CreateProjectBuilder()
+            .WithSourceCode("""
+                using System;
+
+                _ = DateTime.UtcNow - DateTimeOffset.UtcNow;
+                """)
+            .ValidateAsync();
+    }
+
+    [Fact]
+    public async Task ImplicitConversion_BinaryOperation_Subtract()
+    {
+        await CreateProjectBuilder()
+            .WithSourceCode("""
+                using System;
+
+                _ = [|default(DateTime)|] - DateTimeOffset.UtcNow;
+                """)
+            .ValidateAsync();
+    }
+
+    [Fact]
+    public async Task ExplicitConversion_BinaryOperation_Subtract()
+    {
+        await CreateProjectBuilder()
+            .WithSourceCode("""
+                using System;
+
+                _ = (DateTimeOffset)default(DateTime) - DateTimeOffset.UtcNow;
+                """)
+            .ValidateAsync();
+    }
+}
