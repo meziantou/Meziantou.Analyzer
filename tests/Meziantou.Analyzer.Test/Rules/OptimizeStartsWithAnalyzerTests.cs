ae5a33266cd7f0cf0e78ec17358a5996af1744d7
Gérald Barré <git@meziantou.net>
2023-04-26 01:00:11 -0400

    MA0089 support string.IndexOf and string.Replace

--- tests/Meziantou.Analyzer.Test/Rules/OptimizeStartsWithAnalyzerTests.cs
+++ tests/Meziantou.Analyzer.Test/Rules/OptimizeStartsWithAnalyzerTests.cs
@@ -11,7 +11,7 @@ public sealed class OptimizeStartsWithAnalyzerTests
     {
         return new ProjectBuilder()
             .WithAnalyzer<OptimizeStartsWithAnalyzer>()
-            .WithTargetFramework(TargetFramework.NetStandard2_1);
+            .WithTargetFramework(TargetFramework.Net7_0);
     }
 
     [Theory]
@@ -23,10 +23,10 @@ public sealed class OptimizeStartsWithAnalyzerTests
     [InlineData(@"""a"", StringComparison.OrdinalIgnoreCase")]
     [InlineData(@"""a"", StringComparison.CurrentCultureIgnoreCase")]
     [InlineData(@"""a"", StringComparison.InvariantCultureIgnoreCase")]
-    [InlineData(@"[|""a""|]")]
+    [InlineData(@"""a""")]
     [InlineData(@"[|""a""|], StringComparison.Ordinal")]
-    [InlineData(@"[|""a""|], StringComparison.CurrentCulture")]
-    [InlineData(@"[|""a""|], StringComparison.InvariantCulture")]
+    [InlineData(@"""a"", StringComparison.CurrentCulture")]
+    [InlineData(@"""a"", StringComparison.InvariantCulture")]
     public async Task StartsWith(string method)
     {
         var sourceCode = @"
@@ -52,10 +52,10 @@ class Test
     [InlineData(@"""a"", StringComparison.OrdinalIgnoreCase")]
     [InlineData(@"""a"", StringComparison.CurrentCultureIgnoreCase")]
     [InlineData(@"""a"", StringComparison.InvariantCultureIgnoreCase")]
-    [InlineData(@"[|""a""|]")]
+    [InlineData(@"""a""")]
     [InlineData(@"[|""a""|], StringComparison.Ordinal")]
-    [InlineData(@"[|""a""|], StringComparison.CurrentCulture")]
-    [InlineData(@"[|""a""|], StringComparison.InvariantCulture")]
+    [InlineData(@"""a"", StringComparison.CurrentCulture")]
+    [InlineData(@"""a"", StringComparison.InvariantCulture")]
     public async Task EndsWith(string method)
     {
         var sourceCode = @"
@@ -65,6 +65,114 @@ class Test
     void A(string str)
     {
         _ = str.EndsWith(" + method + @");
+    }
+}";
+        await CreateProjectBuilder()
+              .WithSourceCode(sourceCode)
+              .ValidateAsync();
+    }
+
+    [Theory]
+    [InlineData(@"""a"", StringComparison.Ordinal")]
+    [InlineData(@"""a"", StringComparison.CurrentCulture")]
+    [InlineData(@"""a"", 1, 2, StringComparison.Ordinal")]
+    [InlineData(@"""a"", 1, StringComparison.Ordinal")]
+    public async Task IndexOf_Report(string method)
+    {
+        var sourceCode = @"
+using System;
+class Test
+{
+    void A(string str)
+    {
+        _ = [||]str.IndexOf(" + method + @");
+    }
+}";
+        await CreateProjectBuilder()
+              .WithSourceCode(sourceCode)
+              .ValidateAsync();
+    }
+    
+    [Theory]
+    [InlineData("null")]
+    [InlineData(@"""""")]
+    [InlineData(@"str")]
+    [InlineData(@"""abc""")]
+    [InlineData(@"""a""")]
+    [InlineData(@"""a"", 1")]
+    [InlineData(@"""a"", 1, 2")]
+    [InlineData(@"""a"", 1, 2, StringComparison.OrdinalIgnoreCase")]
+    [InlineData(@"""a"", 1, StringComparison.OrdinalIgnoreCase")]
+    public async Task IndexOf_NoReport(string method)
+    {
+        var sourceCode = @"
+using System;
+class Test
+{
+    void A(string str)
+    {
+        _ = str.IndexOf(" + method + @");
+    }
+}";
+        await CreateProjectBuilder()
+              .WithSourceCode(sourceCode)
+              .ValidateAsync();
+    }
+    
+    [Theory]
+    [InlineData(@"""a"", StringComparison.OrdinalIgnoreCase")]
+    public async Task IndexOf_NoReport_Netstandard2_0(string method)
+    {
+        var sourceCode = @"
+using System;
+class Test
+{
+    void A(string str)
+    {
+        _ = str.IndexOf(" + method + @");
+    }
+}";
+        await CreateProjectBuilder()
+              .WithSourceCode(sourceCode)
+              .WithTargetFramework(TargetFramework.NetStandard2_0)
+              .ValidateAsync();
+    }
+
+    [Theory]
+    [InlineData(@"""ab"", """"")]
+    [InlineData(@"""ab"", ""c""")]
+    [InlineData(@"""a"", ""bc""")]
+    [InlineData(@"""a"", ""b"", StringComparison.OrdinalIgnoreCase")]
+    [InlineData(@"""a"", ""b"", StringComparison.CurrentCulture")]
+    [InlineData(@"""a"", ""b"", false, null")]
+    public async Task Replace_NoReport(string method)
+    {
+        var sourceCode = @"
+using System;
+class Test
+{
+    void A(string str)
+    {
+        _ = str.Replace(" + method + @");
+    }
+}";
+        await CreateProjectBuilder()
+              .WithSourceCode(sourceCode)
+              .ValidateAsync();
+    }
+
+    [Theory]
+    [InlineData(@"""a"", ""b""")]
+    [InlineData(@"""a"", ""b"", StringComparison.Ordinal")]
+    public async Task Replace_Report(string method)
+    {
+        var sourceCode = @"
+using System;
+class Test
+{
+    void A(string str)
+    {
+        _ = [||]str.Replace(" + method + @");
     }
 }";
         await CreateProjectBuilder()
