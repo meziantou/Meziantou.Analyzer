76efa881e2d77eaf2774e8f41825846c8a362156
Gérald Barré <git@meziantou.net>
2023-05-16 20:35:50 -0400

    Update Microsoft.CodeAnalysis.CSharp.Workspaces to 4.6.0

--- src/Meziantou.Analyzer/Rules/NamedParameterAnalyzer.cs
+++ src/Meziantou.Analyzer/Rules/NamedParameterAnalyzer.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Immutable;
 using System.Globalization;
-using System.Linq.Expressions;
 using System.Reflection;
 using System.Text.RegularExpressions;
 using System.Threading;
