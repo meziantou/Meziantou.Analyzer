ea68c9e0ae6c766b211f051a62d940aa6907c462
Gérald Barré <git@meziantou.net>
2023-07-26 20:33:29 -0400

    Add Meziantou.Analyzer.Annotations package

---
+++ src/Meziantou.Analyzer.Annotations/Annotations.cs
@@ -0,0 +1,27 @@
+﻿#pragma warning disable CS1591
+#pragma warning disable IDE0060
+#pragma warning disable IDE0290
+#pragma warning disable CA1019
+#nullable disable
+
+namespace Meziantou.Analyzer.Annotations
+{
+    /// <summary>
+    /// Indicates arguments must be named for this parameter.
+    /// </summary>
+    [System.Diagnostics.Conditional("MEZIANTOU_ANALYZER_ANNOTATIONS")]
+    [System.AttributeUsage(System.AttributeTargets.Parameter)]
+    public sealed class RequireNamedArgumentAttribute : System.Attribute
+    {
+        public RequireNamedArgumentAttribute() { }
+
+        public RequireNamedArgumentAttribute(bool isRequired) { }
+    }
+
+    [System.Diagnostics.Conditional("MEZIANTOU_ANALYZER_ANNOTATIONS")]
+    [System.AttributeUsage(System.AttributeTargets.Assembly, AllowMultiple = true, Inherited = false)]
+    public sealed class StructuredLogFieldAttribute : System.Attribute
+    {
+        public StructuredLogFieldAttribute(string parameterName, params System.Type[] allowedTypes) { }
+    }
+}
