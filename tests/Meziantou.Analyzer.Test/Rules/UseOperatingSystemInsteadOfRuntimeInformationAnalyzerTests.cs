cb4682dc07fd873d884ba7a28af70f24899b120c
Gérald Barré <git@meziantou.net>
2023-11-29 00:10:51 -0500

    New rule MA0144: Use System.OperatingSystem to check the current OS

---
+++ tests/Meziantou.Analyzer.Test/Rules/UseOperatingSystemInsteadOfRuntimeInformationAnalyzerTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using Meziantou.Analyzer.Rules;
+using TestHelper;
+using Xunit;
+
+namespace Meziantou.Analyzer.Test.Rules;
+public class UseOperatingSystemInsteadOfRuntimeInformationAnalyzerTests
+{
+    private static ProjectBuilder CreateProjectBuilder()
+    {
+        return new ProjectBuilder()
+            .WithAnalyzer<UseOperatingSystemInsteadOfRuntimeInformationAnalyzer>()
+            .WithTargetFramework(TargetFramework.Net8_0)
+            .WithOutputKind(Microsoft.CodeAnalysis.OutputKind.ConsoleApplication);
+    }
+
+    [Fact]
+    public async Task ShouldReport()
+    {
+        await CreateProjectBuilder()
+            .WithSourceCode("""
+                [||]System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Windows);
+                """)
+            .ValidateAsync();
+    }
+
+    [Fact]
+    public async Task ShouldNotReport_WhenDynamic()
+    {
+        await CreateProjectBuilder()
+            .WithSourceCode("""
+                var a = System.Runtime.InteropServices.OSPlatform.Windows;
+                System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(a);
+                """)
+            .ValidateAsync();
+    }
+}
