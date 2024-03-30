eee141201dec80be2b66cc2d19068f7d833cac53
Julien Richard <julien.richard@ubisoft.com>
2024-03-30 13:12:08 +0100

    UseShellExecute analyzer

---
+++ src/Meziantou.Analyzer/Rules/UseShellExecuteAnalyzer.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace Meziantou.Analyzer.Rules;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public sealed class UseShellExecuteAnalyzer : DiagnosticAnalyzer
+{
+    private static readonly DiagnosticDescriptor UseShellExecuteMustBeExplicitlySet = new(
+        RuleIdentifiers.UseShellExecuteMustBeSet,
+        title: "UseShellExecute must be explicitly set",
+        messageFormat: "UseShellExecute must be explicitly set when initializing a ProcessStartInfo",
+        RuleCategories.Usage,
+        DiagnosticSeverity.Warning,
+        isEnabledByDefault: true,
+        description: "",
+        helpLinkUri: RuleIdentifiers.GetHelpUri(RuleIdentifiers.UseShellExecuteMustBeSet));
+
+    private static readonly DiagnosticDescriptor UseProcessStartOverload = new(
+        RuleIdentifiers.UseShellExecuteMustBeSet,
+        title: "UseShellExecute must be explicitly set",
+        messageFormat: "Use an overload of Process.Start that has a ProcessStartInfo parameter",
+        RuleCategories.Usage,
+        DiagnosticSeverity.Warning,
+        isEnabledByDefault: true,
+        description: "",
+        helpLinkUri: RuleIdentifiers.GetHelpUri(RuleIdentifiers.UseShellExecuteMustBeSet));
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics =>
+        ImmutableArray.Create(UseShellExecuteMustBeExplicitlySet);
+
+    public override void Initialize(AnalysisContext context)
+    {
+        context.EnableConcurrentExecution();
+        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+
+        context.RegisterCompilationStartAction(ctx =>
+        {
+            var analyzerContext = new AnalyzerContext(ctx.Compilation);
+            if (!analyzerContext.IsValid)
+                return;
+
+            ctx.RegisterOperationAction(analyzerContext.AnalyzeInvocation, OperationKind.Invocation);
+            ctx.RegisterOperationAction(analyzerContext.AnalyzeObjectCreation, OperationKind.ObjectCreation);
+        });
+    }
+
+    private sealed class AnalyzerContext(Compilation compilation)
+    {
+        private readonly INamedTypeSymbol? _processStartInfoSymbol =
+            compilation.GetBestTypeByMetadataName("System.Diagnostics.ProcessStartInfo")!;
+
+        private readonly INamedTypeSymbol? _processSymbol =
+            compilation.GetBestTypeByMetadataName("System.Diagnostics.Process")!;
+
+        public bool IsValid => _processStartInfoSymbol is not null;
+
+        public void AnalyzeInvocation(OperationAnalysisContext context)
+        {
+            var operation = (IInvocationOperation)context.Operation;
+            if (IsProcessStartInvocation(operation))
+            {
+                if (!operation.Arguments.Any(IsProcessStartInfo))
+                {
+                    // Calling Process.Start without ProcessStartInfo
+                    context.ReportDiagnostic(UseProcessStartOverload, operation.Syntax);
+                }
+            }
+        }
+
+        public void AnalyzeObjectCreation(OperationAnalysisContext context)
+        {
+            var operation = (IObjectCreationOperation)context.Operation;
+            if (IsProcessStartInfoCreation(operation))
+            {
+                if (operation.Syntax is ObjectCreationExpressionSyntax { Initializer: {} initializer } )
+                {
+                    var useShellExecuteInitializer = initializer.Expressions.OfType<AssignmentExpressionSyntax>()
+                        .FirstOrDefault(x => x.Left is IdentifierNameSyntax
+                        {
+                            Identifier.Text: "UseShellExecute"
+                        });
+
+                    if (useShellExecuteInitializer is null)
+                    {
+                        // Constructing ProcessStartInfo without setting UseShellExecute in the initializer
+                        context.ReportDiagnostic(UseShellExecuteMustBeExplicitlySet, operation.Syntax);
+                    }
+                }
+                else
+                {
+                    // Constructing ProcessStartInfo with not initializer at all
+                    context.ReportDiagnostic(UseShellExecuteMustBeExplicitlySet, operation.Syntax);
+                }
+            }
+        }
+
+        private bool IsProcessStartInfo(IArgumentOperation operation)
+            => operation.Value.Type.IsEqualTo(_processStartInfoSymbol);
+
+        private bool IsProcessStartInfoCreation(IObjectCreationOperation operation)
+            => operation.Type.IsEqualTo(_processStartInfoSymbol);
+
+        private bool IsProcessStartInvocation(IInvocationOperation operation)
+            => operation.TargetMethod.Name == "Start" && operation.TargetMethod.ContainingType.IsEqualTo(_processSymbol);
+
+    }
+}
---
+++ tests/Meziantou.Analyzer.Test/Rules/UseShellExecuteAnalyzerTests.cs
@@ -0,0 +1,141 @@
+﻿using System.Threading.Tasks;
+using Meziantou.Analyzer.Rules;
+using TestHelper;
+using Xunit;
+
+namespace Meziantou.Analyzer.Test.Rules;
+
+public sealed class UseShellExecuteAnalyzerTests
+{
+    private static ProjectBuilder CreateProjectBuilder()
+    {
+        return new ProjectBuilder()
+            .WithAnalyzer<UseShellExecuteAnalyzer>("MA0155");
+    }
+
+    [Fact]
+    public async Task Process_start_should_not_report_when_use_shell_execute_is_set_to_false()
+    {
+        const string SourceCode = """
+                                  using System.Diagnostics;
+
+                                  class TypeName
+                                  {
+                                      public void Test()
+                                      {
+                                          Process.Start(new ProcessStartInfo { UseShellExecute = false });
+                                      }
+                                  }
+                                  """;
+        await CreateProjectBuilder()
+            .WithSourceCode(SourceCode)
+            .ValidateAsync();
+    }
+
+    [Fact]
+    public async Task Process_start_should_not_report_when_use_shell_execute_is_set_to_true()
+    {
+        const string SourceCode = """
+                                  using System.Diagnostics;
+
+                                  class TypeName
+                                  {
+                                      public void Test()
+                                      {
+                                          Process.Start(new ProcessStartInfo { UseShellExecute = true });
+                                      }
+                                  }
+                                  """;
+        await CreateProjectBuilder()
+            .WithSourceCode(SourceCode)
+            .ValidateAsync();
+    }
+
+    [Fact]
+    public async Task Process_start_should_report_when_use_shell_execute_is_not_set()
+    {
+        const string SourceCode = """
+                                  using System.Diagnostics;
+
+                                  class TypeName
+                                  {
+                                      public void Test()
+                                      {
+                                          var processStartInfo = [||]new ProcessStartInfo();
+                                          Process.Start(processStartInfo);
+                                      }
+                                  }
+                                  """;
+        await CreateProjectBuilder()
+            .WithSourceCode(SourceCode)
+            .ShouldReportDiagnosticWithMessage("UseShellExecute must be explicitly set when initializing a ProcessStartInfo")
+            .ValidateAsync();
+    }
+
+    [Fact]
+    public async Task Process_start_should_report_false_positives()
+    {
+        const string SourceCode = """
+                                  using System.Diagnostics;
+
+                                  class TypeName
+                                  {
+                                      public void Test()
+                                      {
+                                          var processStartInfo = [||]new ProcessStartInfo();
+                                          processStartInfo.UseShellExecute = false;
+                                          Process.Start(processStartInfo);
+                                      }
+                                  }
+                                  """;
+        await CreateProjectBuilder()
+            .WithSourceCode(SourceCode)
+            .ShouldReportDiagnosticWithMessage("UseShellExecute must be explicitly set when initializing a ProcessStartInfo")
+            .ValidateAsync();
+    }
+
+    [Fact]
+    public async Task Process_start_should_report_when_use_shell_execute_is_not_set_2()
+    {
+        const string SourceCode = """
+                                  using System.Diagnostics;
+
+                                  class TypeName
+                                  {
+                                      public void Test()
+                                      {
+                                          var processStartInfo = [||]new ProcessStartInfo()
+                                          {
+                                              FileName = "notepad",
+                                          };
+                                          Process.Start(processStartInfo);
+                                      }
+                                  }
+                                  """;
+        await CreateProjectBuilder()
+            .WithSourceCode(SourceCode)
+            .ShouldReportDiagnosticWithMessage("UseShellExecute must be explicitly set when initializing a ProcessStartInfo")
+            .ValidateAsync();
+    }
+
+
+    [Fact]
+    public async Task Process_start_should_report_when_using_overload_with_no_process_start_info()
+    {
+        const string SourceCode = """
+                                  using System.Diagnostics;
+
+                                  class TypeName
+                                  {
+                                      public void Test()
+                                      {
+                                          [||]Process.Start("notepad");
+                                      }
+                                  }
+                                  """;
+        await CreateProjectBuilder()
+            .WithSourceCode(SourceCode)
+            .ShouldReportDiagnosticWithMessage("Use an overload of Process.Start that has a ProcessStartInfo parameter")
+            .ValidateAsync();
+    }
+}
