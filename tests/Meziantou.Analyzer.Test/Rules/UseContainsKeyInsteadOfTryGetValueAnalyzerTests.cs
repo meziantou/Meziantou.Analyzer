fe0577bc5c2282ae6193bf19ee0f0a53b32e264d
Gérald Barré <git@meziantou.net>
2024-07-16 18:10:03 -0400

    New rule: MA0160 Use ContainsKey instead of TryGetValue when the second arg is a discard

---
+++ tests/Meziantou.Analyzer.Test/Rules/UseContainsKeyInsteadOfTryGetValueAnalyzerTests.cs
@@ -0,0 +1,98 @@
+﻿using System.Threading.Tasks;
+using Meziantou.Analyzer.Rules;
+using TestHelper;
+using Xunit;
+
+namespace Meziantou.Analyzer.Test.Rules;
+public sealed class UseContainsKeyInsteadOfTryGetValueAnalyzerTests
+{
+    private static ProjectBuilder CreateProjectBuilder()
+    {
+        return new ProjectBuilder()
+            .WithAnalyzer<UseContainsKeyInsteadOfTryGetValueAnalyzer>();
+    }
+
+    [Fact]
+    public async Task IDictionary_TryGetValue_Value()
+    {
+        await CreateProjectBuilder()
+              .WithSourceCode("""
+                class ClassTest
+                {
+                    void Test(System.Collections.Generic.IDictionary<string, string> dict)
+                    {
+                        dict.TryGetValue("", out var a);
+                    }
+                }
+                """)
+              .ValidateAsync();
+    }
+
+    [Fact]
+    public async Task IDictionary_TryGetValue_Discard()
+    {
+        await CreateProjectBuilder()
+              .WithSourceCode("""
+                class ClassTest
+                {
+                    void Test(System.Collections.Generic.IDictionary<string, string> dict)
+                    {
+                        [||]dict.TryGetValue("", out _);
+                    }
+                }
+                """)
+              .ValidateAsync();
+    }
+
+    [Fact]
+    public async Task IReadOnlyDictionary_TryGetValue_Discard()
+    {
+        await CreateProjectBuilder()
+              .WithSourceCode("""
+                class ClassTest
+                {
+                    void Test(System.Collections.Generic.IReadOnlyDictionary<string, string> dict)
+                    {
+                        [||]dict.TryGetValue("", out _);
+                    }
+                }
+                """)
+              .ValidateAsync();
+    }
+
+    [Fact]
+    public async Task Dictionary_TryGetValue_Discard()
+    {
+        await CreateProjectBuilder()
+              .WithSourceCode("""
+                class ClassTest
+                {
+                    void Test(System.Collections.Generic.Dictionary<string, string> dict)
+                    {
+                        [||]dict.TryGetValue("", out _);
+                    }
+                }
+                """)
+              .ValidateAsync();
+    }
+
+    [Fact]
+    public async Task CustomDictionary_TryGetValue_Discard()
+    {
+        await CreateProjectBuilder()
+              .WithSourceCode("""
+                class ClassTest
+                {
+                    void Test(SampleDictionary dict)
+                    {
+                        [||]dict.TryGetValue("", out _);
+                    }
+                }
+
+                class SampleDictionary : System.Collections.Generic.Dictionary<string, string>
+                {
+                }
+                """)
+              .ValidateAsync();
+    }
+}
