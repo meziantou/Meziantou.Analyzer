d7c50bf40aa70d0cbf400bccba4450680f5cea10
Gérald Barré <meziantou@users.noreply.github.com>
2025-08-26 10:26:26 -0400

    Add failing test for constructor in abstract class

--- tests/Meziantou.Analyzer.Test/Rules/AbstractTypesShouldNotHaveConstructorsAnalyzerTests.cs
+++ tests/Meziantou.Analyzer.Test/Rules/AbstractTypesShouldNotHaveConstructorsAnalyzerTests.cs
@@ -17,6 +17,7 @@ public sealed class AbstractTypesShouldNotHaveConstructorsAnalyzerTests
     [Fact]
     public async Task Ctor()
     {
+        Assert.Fail("test");
         const string SourceCode = @"
 abstract class Test
 {
