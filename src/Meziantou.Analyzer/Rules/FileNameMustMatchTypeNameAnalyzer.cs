949334f9b46efef4e428a751ef4cd27d78826a93
Meir Blachman <meirblachman@gmail.com>
2025-09-14 23:56:18 +0300

    Update type kind display string checks for C# 10 compatibility

--- src/Meziantou.Analyzer/Rules/FileNameMustMatchTypeNameAnalyzer.cs
+++ src/Meziantou.Analyzer/Rules/FileNameMustMatchTypeNameAnalyzer.cs
@@ -159,11 +159,11 @@ public sealed class FileNameMustMatchTypeNameAnalyzer : DiagnosticAnalyzer
     {
         return symbol.TypeKind switch
         {
-#if ROSLYN_4_2_OR_GREATER
+#if CSHARP10_OR_GREATER
             TypeKind.Class when symbol.IsRecord => "record",
 #endif
             TypeKind.Class => "class",
-#if ROSLYN_4_2_OR_GREATER
+#if CSHARP10_OR_GREATER
             TypeKind.Struct when symbol.IsRecord => "record struct",
 #endif
             TypeKind.Struct => "struct",
