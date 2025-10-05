17d4f61c4707519fe43e7d110b25e5aac45bdeff
Gérald Barré <meziantou@users.noreply.github.com>
2025-10-05 16:39:40 -0400

    New rule: MA0176 - Optimize guid creation (#884)

---
+++ src/Meziantou.Analyzer/Rules/OptimizeGuidCreationAnalyzer.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Immutable;
+using Meziantou.Analyzer.Internals;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace Meziantou.Analyzer.Rules;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public sealed class OptimizeGuidCreationAnalyzer : DiagnosticAnalyzer
+{
+    private static readonly DiagnosticDescriptor Rule = new(
+        RuleIdentifiers.OptimizeGuidCreation,
+        title: "Optimize guid creation",
+        messageFormat: "Optimize guid creation",
+        RuleCategories.Performance,
+        DiagnosticSeverity.Info,
+        isEnabledByDefault: true,
+        description: "",
+        helpLinkUri: RuleIdentifiers.GetHelpUri(RuleIdentifiers.OptimizeGuidCreation));
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+    public override void Initialize(AnalysisContext context)
+    {
+        context.EnableConcurrentExecution();
+        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.ReportDiagnostics);
+
+        context.RegisterCompilationStartAction(ctx =>
+        {
+            var type = ctx.Compilation.GetBestTypeByMetadataName("System.Guid");
+            if (type is null)
+                return;
+
+            ctx.RegisterOperationAction(symbolContext => AnalyzeInvocation(symbolContext, type), OperationKind.Invocation); // Guid.Parse(""), Guid.TryParse("")
+            ctx.RegisterOperationAction(symbolContext => AnalyzeObjectCreation(symbolContext, type), OperationKind.ObjectCreation); // new Guid("")
+        });
+    }
+
+    private static void AnalyzeObjectCreation(OperationAnalysisContext symbolContext, INamedTypeSymbol type)
+    {
+        var creation = (IObjectCreationOperation)symbolContext.Operation;
+        if (creation.Constructor is null)
+            return;
+
+        if (!creation.Constructor.ContainingType.IsEqualTo(type))
+            return;
+
+        if (creation is { Arguments: [{ Value.Type.SpecialType: SpecialType.System_String, Value.ConstantValue: { HasValue: true, Value: string value } }] })
+        {
+            if (Guid.TryParse(value, out _))
+            {
+                symbolContext.ReportDiagnostic(Rule, creation);
+            }
+        }
+    }
+
+    private static void AnalyzeInvocation(OperationAnalysisContext context, INamedTypeSymbol guidType)
+    {
+        var invocation = (IInvocationOperation)context.Operation;
+        if (!invocation.TargetMethod.ContainingType.IsEqualTo(guidType))
+            return;
+
+        if (invocation is { TargetMethod.Name: "Parse", Arguments: [{ Value.Type.SpecialType: SpecialType.System_String, Value.ConstantValue: { HasValue: true, Value: string value } }] })
+        {
+            if (Guid.TryParse(value, out _))
+            {
+                context.ReportDiagnostic(Rule, invocation);
+            }
+        }
+    }
+}
---
+++ tests/Meziantou.Analyzer.Test/Rules/OptimizeGuidParsingAnalyzerTests.cs
@@ -0,0 +1,63 @@
+﻿using Meziantou.Analyzer.Rules;
+using TestHelper;
+using Xunit;
+
+namespace Meziantou.Analyzer.Test.Rules;
+
+public sealed class OptimizeGuidParsingAnalyzerTests
+{
+    private static ProjectBuilder CreateProjectBuilder()
+    {
+        return new ProjectBuilder()
+            .WithAnalyzer<OptimizeGuidCreationAnalyzer>()
+            .WithCodeFixProvider<OptimizeGuidCreationFixer>()
+            .WithOutputKind(Microsoft.CodeAnalysis.OutputKind.ConsoleApplication);
+    }
+
+    [Fact]
+    public async Task CtorConstantString()
+    {
+        await CreateProjectBuilder()
+              .WithSourceCode("""
+                 _ = [|new System.Guid("10752bc4-c151-50f5-f27b-df92d8af5a61")|];
+                 """)
+              .ShouldFixCodeWith("""
+                 _ = new System.Guid(0x10752bc4, 0xc151, 0x50f5, 0xf2, 0x7b, 0xdf, 0x92, 0xd8, 0xaf, 0x5a, 0x61) /* 10752bc4-c151-50f5-f27b-df92d8af5a61 */;
+                 """)
+              .ValidateAsync();
+    }
+
+    [Fact]
+    public async Task ParseConstantString()
+    {
+        await CreateProjectBuilder()
+              .WithSourceCode("""
+                 _ = [|System.Guid.Parse("10752BC4-C151-50F5-F27B-DF92D8AF5A61")|];
+                 """)
+              .ShouldFixCodeWith("""
+                 _ = new System.Guid(0x10752BC4, 0xC151, 0x50F5, 0xF2, 0x7B, 0xDF, 0x92, 0xD8, 0xAF, 0x5A, 0x61) /* 10752BC4-C151-50F5-F27B-DF92D8AF5A61 */;
+                 """)
+              .ValidateAsync();
+    }
+
+    [Fact]
+    public async Task ParseNonConstantString()
+    {
+        await CreateProjectBuilder()
+              .WithSourceCode("""
+                 var value = "10752BC4-C151-50F5-F27B-DF92D8AF5A61";
+                 _ = System.Guid.Parse(value);
+                 """)
+              .ValidateAsync();
+    }
+
+    [Fact]
+    public async Task ParseInvalidGuid()
+    {
+        await CreateProjectBuilder()
+              .WithSourceCode("""
+                 _ = System.Guid.Parse("dummy");
+                 """)
+              .ValidateAsync();
+    }
+}
