b425e81023e5a5b7b3965ff944547730388c245a
Gérald Barré <git@meziantou.net>
2026-07-27 01:24:20 +0200

    wip

--- src/Meziantou.Analyzer/Internals/LanguageVersionExtensions.cs
+++ src/Meziantou.Analyzer/Internals/LanguageVersionExtensions.cs
@@ -31,7 +31,11 @@ internal static class LanguageVersionExtensions
 
     public static bool IsCSharp15OrAbove(this LanguageVersion languageVersion)
     {
+#if CSHARP15_OR_GREATER
         return languageVersion >= (LanguageVersion)1500 || languageVersion is LanguageVersion.Preview;
+#else
+        return false;
+#endif
     }
 
     public static bool IsCSharp8OrAbove(this LanguageVersion languageVersion)
