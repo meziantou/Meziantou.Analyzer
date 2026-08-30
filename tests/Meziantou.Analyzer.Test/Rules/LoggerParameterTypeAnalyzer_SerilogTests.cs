b6a4406af411e463acc62a80f4306f59619b76e8
Gérald Barré <git@meziantou.net>
2026-08-30 16:24:38 -0400

    wip

--- tests/Meziantou.Analyzer.Test/Rules/LoggerParameterTypeAnalyzer_SerilogTests.cs
+++ tests/Meziantou.Analyzer.Test/Rules/LoggerParameterTypeAnalyzer_SerilogTests.cs
@@ -1,291 +1,298 @@
+using System.Diagnostics;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Testing;
+using AnalyzerTest = Meziantou.Analyzer.Test.Harness.CSharpAnalyzerTest<
+    Meziantou.Analyzer.Rules.LoggerParameterTypeAnalyzer>;
+
 namespace Meziantou.Analyzer.Test.Rules;
+
 public sealed class LoggerParameterTypeAnalyzer_SerilogTests
 {
-    private static ProjectBuilder CreateProjectBuilder() => new ProjectBuilder()
-            .WithAnalyzer<LoggerParameterTypeAnalyzer>()
-            .WithOutputKind(Microsoft.CodeAnalysis.OutputKind.ConsoleApplication)
-            .WithLanguageVersion(Microsoft.CodeAnalysis.CSharp.LanguageVersion.Latest)
-            .WithTargetFramework(TargetFramework.Net8_0)
-            .AddNuGetReference("Serilog", "3.1.1", "lib/net7.0");
-
-    [Fact]
-    public async Task SeriLog_Log_Information()
+    private static AnalyzerTest CreateTest()
     {
-        const string SourceCode = """
-using Serilog;
-
-Log.Information("{Prop}", 1);
-Log.Information("{Prop}", {|MA0139:(int?)1|});
-""";
-        await CreateProjectBuilder()
-              .WithSourceCode(SourceCode)
-              .AddAdditionalFile("LoggerParameterTypes.txt", """
-Prop;System.Int32
-""")
-              .ValidateAsync();
+        var test = new AnalyzerTest();
+        test.ReferenceAssemblies = test.ReferenceAssemblies.AddPackages([new PackageIdentity("Serilog", "3.1.1")]);
+        test.TestState.OutputKind = OutputKind.ConsoleApplication;
+        return test;
     }
 
     [Fact]
-    public async Task SeriLog_Log_Information_Exception()
+    public Task SeriLog_Log_Information()
     {
-        const string SourceCode = """
-using Serilog;
+        var test = CreateTest();
+        test.TestCode = """
+            using Serilog;
 
-Log.Information((System.Exception)null, "{Prop}", 1);
-Log.Information((System.Exception)null, "{Prop}", {|MA0139:(int?)1|});
-""";
-        await CreateProjectBuilder()
-              .WithSourceCode(SourceCode)
-              .AddAdditionalFile("LoggerParameterTypes.txt", """
-Prop;System.Int32
-""")
-              .ValidateAsync();
+            Log.Information("{Prop}", 1);
+            Log.Information("{Prop}", {|MA0139:(int?)1|});
+            """;
+        test.TestState.AdditionalFiles.Add(("LoggerParameterTypes.txt", """
+            Prop;System.Int32
+            """));
+
+        return test.RunAsync();
     }
 
     [Fact]
-    public async Task SeriLog_Log_Information_Params()
+    public Task SeriLog_Log_Information_Exception()
     {
-        const string SourceCode = """
-using Serilog;
+        var test = CreateTest();
+        test.TestCode = """
+            using Serilog;
 
-Log.Information("{Prop}{Prop}{Prop}{Prop}", 1, 1, 1, 1);
-""";
-        await CreateProjectBuilder()
-              .WithSourceCode(SourceCode)
-              .AddAdditionalFile("LoggerParameterTypes.txt", """
-Prop;System.Int32
-""")
-              .ValidateAsync();
+            Log.Information((System.Exception)null, "{Prop}", 1);
+            Log.Information((System.Exception)null, "{Prop}", {|MA0139:(int?)1|});
+            """;
+        test.TestState.AdditionalFiles.Add(("LoggerParameterTypes.txt", """
+            Prop;System.Int32
+            """));
+
+        return test.RunAsync();
     }
 
     [Fact]
-    public async Task SeriLog_Log_Information_AtPrefix()
+    public Task SeriLog_Log_Information_Params()
     {
-        const string SourceCode = """
-using Serilog;
+        var test = CreateTest();
+        test.TestCode = """
+            using Serilog;
 
-Log.Information("{@Prop}", 1);
-""";
-        await CreateProjectBuilder()
-              .WithSourceCode(SourceCode)
-              .AddAdditionalFile("LoggerParameterTypes.txt", """
-Prop;System.Int32
-""")
-              .ValidateAsync();
+            Log.Information("{Prop}{Prop}{Prop}{Prop}", 1, 1, 1, 1);
+            """;
+        test.TestState.AdditionalFiles.Add(("LoggerParameterTypes.txt", """
+            Prop;System.Int32
+            """));
+
+        return test.RunAsync();
     }
 
     [Fact]
-    public async Task SeriLog_Log_Information_DollarPrefix()
+    public Task SeriLog_Log_Information_AtPrefix()
     {
-        const string SourceCode = """
-using Serilog;
+        var test = CreateTest();
+        test.TestCode = """
+            using Serilog;
 
-Log.Information("{$Prop}", 1);
-""";
-        await CreateProjectBuilder()
-              .WithSourceCode(SourceCode)
-              .AddAdditionalFile("LoggerParameterTypes.txt", """
-Prop;System.Int32
-""")
-              .ValidateAsync();
+            Log.Information("{@Prop}", 1);
+            """;
+        test.TestState.AdditionalFiles.Add(("LoggerParameterTypes.txt", """
+            Prop;System.Int32
+            """));
+
+        return test.RunAsync();
     }
 
     [Fact]
-    public async Task SeriLog_Log_Information_AtPrefix_MultipleParams()
+    public Task SeriLog_Log_Information_DollarPrefix()
     {
-        const string SourceCode = """
-using Serilog;
+        var test = CreateTest();
+        test.TestCode = """
+            using Serilog;
 
-Log.Information("{@Prop1}{@Prop2}", 1, 2);
-""";
-        await CreateProjectBuilder()
-              .WithSourceCode(SourceCode)
-              .AddAdditionalFile("LoggerParameterTypes.txt", """
-Prop1;System.Int32
-Prop2;System.Int32
-""")
-              .ValidateAsync();
+            Log.Information("{$Prop}", 1);
+            """;
+        test.TestState.AdditionalFiles.Add(("LoggerParameterTypes.txt", """
+            Prop;System.Int32
+            """));
+
+        return test.RunAsync();
     }
 
     [Fact]
-    public async Task SeriLog_Log_Information_AtPrefix_MixedParams()
+    public Task SeriLog_Log_Information_AtPrefix_MultipleParams()
     {
-        const string SourceCode = """
-using Serilog;
+        var test = CreateTest();
+        test.TestCode = """
+            using Serilog;
 
-Log.Information("{Bar}{@Prop}", 1, 2);
-""";
-        await CreateProjectBuilder()
-              .WithSourceCode(SourceCode)
-              .AddAdditionalFile("LoggerParameterTypes.txt", """
-Bar;System.Int32
-Prop;System.Int32
-""")
-              .ValidateAsync();
+            Log.Information("{@Prop1}{@Prop2}", 1, 2);
+            """;
+        test.TestState.AdditionalFiles.Add(("LoggerParameterTypes.txt", """
+            Prop1;System.Int32
+            Prop2;System.Int32
+            """));
+
+        return test.RunAsync();
     }
 
     [Fact]
-    public async Task SeriLog_ILogger_AtPrefix_MultipleParams()
+    public Task SeriLog_Log_Information_AtPrefix_MixedParams()
     {
-        const string SourceCode = """
-using Serilog;
+        var test = CreateTest();
+        test.TestCode = """
+            using Serilog;
 
-Serilog.ILogger logger = null!;
-logger.Debug("{@Prop1}{@Prop2}", 1, 2);
-""";
-        await CreateProjectBuilder()
-              .WithSourceCode(SourceCode)
-              .AddAdditionalFile("LoggerParameterTypes.txt", """
-Prop1;System.Int32
-Prop2;System.Int32
-""")
-              .ValidateAsync();
+            Log.Information("{Bar}{@Prop}", 1, 2);
+            """;
+        test.TestState.AdditionalFiles.Add(("LoggerParameterTypes.txt", """
+            Bar;System.Int32
+            Prop;System.Int32
+            """));
+
+        return test.RunAsync();
     }
 
     [Fact]
-    public async Task SeriLog_ILogger_AtPrefix_MixedParams()
+    public Task SeriLog_ILogger_AtPrefix_MultipleParams()
     {
-        const string SourceCode = """
-using Serilog;
+        var test = CreateTest();
+        test.TestCode = """
+            using Serilog;
 
-Serilog.ILogger logger = null!;
-logger.Debug("{Bar}{@Prop}", 1, 2);
-""";
-        await CreateProjectBuilder()
-              .WithSourceCode(SourceCode)
-              .AddAdditionalFile("LoggerParameterTypes.txt", """
-Bar;System.Int32
-Prop;System.Int32
-""")
-              .ValidateAsync();
+            Serilog.ILogger logger = null!;
+            logger.Debug("{@Prop1}{@Prop2}", 1, 2);
+            """;
+        test.TestState.AdditionalFiles.Add(("LoggerParameterTypes.txt", """
+            Prop1;System.Int32
+            Prop2;System.Int32
+            """));
+
+        return test.RunAsync();
     }
 
     [Fact]
-    public async Task SeriLog_Enrich_WithProperty()
+    public Task SeriLog_ILogger_AtPrefix_MixedParams()
     {
-        const string SourceCode = """
-using Serilog;
+        var test = CreateTest();
+        test.TestCode = """
+            using Serilog;
 
-new LoggerConfiguration().Enrich.WithProperty("Prop", 0);
-""";
-        await CreateProjectBuilder()
-              .WithSourceCode(SourceCode)
-              .AddAdditionalFile("LoggerParameterTypes.txt", """
-Prop;System.Int32
-""")
-              .ValidateAsync();
+            Serilog.ILogger logger = null!;
+            logger.Debug("{Bar}{@Prop}", 1, 2);
+            """;
+        test.TestState.AdditionalFiles.Add(("LoggerParameterTypes.txt", """
+            Bar;System.Int32
+            Prop;System.Int32
+            """));
+
+        return test.RunAsync();
     }
 
     [Fact]
-    public async Task SeriLog_Enrich_WithProperty_Invalid()
+    public Task SeriLog_Enrich_WithProperty()
     {
-        const string SourceCode = """
-using Serilog;
+        var test = CreateTest();
+        test.TestCode = """
+            using Serilog;
 
-new LoggerConfiguration().Enrich.WithProperty("Prop", [|""|]);
-""";
-        await CreateProjectBuilder()
-              .WithDefaultAnalyzerId("MA0139")
-              .WithSourceCode(SourceCode)
-              .AddAdditionalFile("LoggerParameterTypes.txt", """
-Prop;System.Int32
-""")
-              .ValidateAsync();
+            new LoggerConfiguration().Enrich.WithProperty("Prop", 0);
+            """;
+        test.TestState.AdditionalFiles.Add(("LoggerParameterTypes.txt", """
+            Prop;System.Int32
+            """));
+
+        return test.RunAsync();
     }
 
     [Fact]
-    public async Task SeriLog_Log_ForContext()
+    public Task SeriLog_Enrich_WithProperty_Invalid()
     {
-        const string SourceCode = """
-using Serilog;
+        var test = CreateTest();
+        test.TestCode = """
+            using Serilog;
 
-Log.ForContext("Prop", 0);
-""";
-        await CreateProjectBuilder()
-              .WithSourceCode(SourceCode)
-              .AddAdditionalFile("LoggerParameterTypes.txt", """
-Prop;System.Int32
-""")
-              .ValidateAsync();
+            new LoggerConfiguration().Enrich.WithProperty("Prop", {|MA0139:""|});
+            """;
+        test.TestState.AdditionalFiles.Add(("LoggerParameterTypes.txt", """
+            Prop;System.Int32
+            """));
+
+        return test.RunAsync();
     }
 
     [Fact]
-    public async Task SeriLog_Log_ForContext_Invalid()
+    public Task SeriLog_Log_ForContext()
     {
-        const string SourceCode = """
-using Serilog;
+        var test = CreateTest();
+        test.TestCode = """
+            using Serilog;
 
-Log.ForContext("Prop", {|MA0139:""|});
-""";
-        await CreateProjectBuilder()
-              .WithSourceCode(SourceCode)
-              .AddAdditionalFile("LoggerParameterTypes.txt", """
-Prop;System.Int32
-""")
-              .ValidateAsync();
+            Log.ForContext("Prop", 0);
+            """;
+        test.TestState.AdditionalFiles.Add(("LoggerParameterTypes.txt", """
+            Prop;System.Int32
+            """));
+
+        return test.RunAsync();
     }
 
     [Fact]
-    public async Task SeriLog_ILogger_ForContext()
+    public Task SeriLog_Log_ForContext_Invalid()
     {
-        const string SourceCode = """
-using Serilog;
+        var test = CreateTest();
+        test.TestCode = """
+            using Serilog;
 
-Log.Logger.ForContext("Prop", 0);
-""";
-        await CreateProjectBuilder()
-              .WithSourceCode(SourceCode)
-              .AddAdditionalFile("LoggerParameterTypes.txt", """
-Prop;System.Int32
-""")
-              .ValidateAsync();
+            Log.ForContext("Prop", {|MA0139:""|});
+            """;
+        test.TestState.AdditionalFiles.Add(("LoggerParameterTypes.txt", """
+            Prop;System.Int32
+            """));
+
+        return test.RunAsync();
     }
 
     [Fact]
-    public async Task SeriLog_ILogger_ForContext_Invalid()
+    public Task SeriLog_ILogger_ForContext()
     {
-        const string SourceCode = """
-using Serilog;
+        var test = CreateTest();
+        test.TestCode = """
+            using Serilog;
 
-Log.Logger.ForContext("Prop", {|MA0139:""|});
-""";
-        await CreateProjectBuilder()
-              .WithSourceCode(SourceCode)
-              .AddAdditionalFile("LoggerParameterTypes.txt", """
-Prop;System.Int32
-""")
-              .ValidateAsync();
+            Log.Logger.ForContext("Prop", 0);
+            """;
+        test.TestState.AdditionalFiles.Add(("LoggerParameterTypes.txt", """
+            Prop;System.Int32
+            """));
+
+        return test.RunAsync();
     }
 
     [Fact]
-    public async Task SeriLog_ILogger_ForContext_LogEventLevel()
+    public Task SeriLog_ILogger_ForContext_Invalid()
     {
-        const string SourceCode = """
-using Serilog;
+        var test = CreateTest();
+        test.TestCode = """
+            using Serilog;
 
-Log.Logger.ForContext(Serilog.Events.LogEventLevel.Warning, "Prop", 0);
-""";
-        await CreateProjectBuilder()
-              .WithSourceCode(SourceCode)
-              .AddAdditionalFile("LoggerParameterTypes.txt", """
-Prop;System.Int32
-""")
-              .ValidateAsync();
+            Log.Logger.ForContext("Prop", {|MA0139:""|});
+            """;
+        test.TestState.AdditionalFiles.Add(("LoggerParameterTypes.txt", """
+            Prop;System.Int32
+            """));
+
+        return test.RunAsync();
     }
 
     [Fact]
-    public async Task SeriLog_ILogger_ForContext_LogEventLevel_Invalid()
+    public Task SeriLog_ILogger_ForContext_LogEventLevel()
     {
-        const string SourceCode = """
-using Serilog;
+        var test = CreateTest();
+        test.TestCode = """
+            using Serilog;
 
-Log.Logger.ForContext(Serilog.Events.LogEventLevel.Warning,"Prop", {|MA0139:""|});
-""";
-        await CreateProjectBuilder()
-              .WithSourceCode(SourceCode)
-              .AddAdditionalFile("LoggerParameterTypes.txt", """
-Prop;System.Int32
-""")
-              .ValidateAsync();
+            Log.Logger.ForContext(Serilog.Events.LogEventLevel.Warning, "Prop", 0);
+            """;
+        test.TestState.AdditionalFiles.Add(("LoggerParameterTypes.txt", """
+            Prop;System.Int32
+            """));
+
+        return test.RunAsync();
+    }
+
+    [Fact]
+    public Task SeriLog_ILogger_ForContext_LogEventLevel_Invalid()
+    {
+        var test = CreateTest();
+        test.TestCode = """
+            using Serilog;
+
+            Log.Logger.ForContext(Serilog.Events.LogEventLevel.Warning,"Prop", {|MA0139:""|});
+            """;
+        test.TestState.AdditionalFiles.Add(("LoggerParameterTypes.txt", """
+            Prop;System.Int32
+            """));
+
+        return test.RunAsync();
     }
 }
