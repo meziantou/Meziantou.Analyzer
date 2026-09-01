7d3444eef2190b8df8adfc285dfe4bbbcfe451c8
Gérald Barré <git@meziantou.net>
2026-08-31 22:15:47 -0400

    Merge the DoNotUseDefaultEqualsOnValueType test classes
    MA0065 and MA0066 had one test class each, disabling the other rule, even
    though no snippet reported both.

--- tests/Meziantou.Analyzer.Test/Rules/DoNotUseDefaultEqualsOnValueTypeAnalyzerTests.cs
+++ tests/Meziantou.Analyzer.Test/Rules/DoNotUseDefaultEqualsOnValueTypeAnalyzerTests.cs
@@ -3,13 +3,158 @@ using AnalyzerTest = Meziantou.Analyzer.Test.Harness.CSharpAnalyzerTest<
 
 namespace Meziantou.Analyzer.Test.Rules;
 
-public sealed class DoNotUseDefaultEqualsOnValueTypeAnalyzer_HashSetTests
+public sealed class DoNotUseDefaultEqualsOnValueTypeAnalyzerTests
 {
-    private static AnalyzerTest CreateTest()
+    [Fact]
+    public Task Equals_DefaultImplementation()
     {
         var test = new AnalyzerTest();
-        test.DisabledDiagnostics.Add(RuleIdentifiers.DoNotUseDefaultEqualsOnValueType);
-        return test;
+        test.TestCode = """
+            struct Test
+            {
+            }
+
+            class Sample
+            {
+                public void A()
+                {
+                    _ = {|MA0065:new Test().Equals(new Test())|};
+                }
+            }
+            """;
+
+        return test.RunAsync();
+    }
+
+    [Fact]
+    public Task ObjectEquals_DefaultImplementation()
+    {
+        var test = new AnalyzerTest();
+        test.TestCode = """
+            struct Test
+            {
+                public void A()
+                {
+                    _ = {|MA0065:Equals(new Test())|};
+                }
+            }
+            """;
+
+        return test.RunAsync();
+    }
+
+    [Fact]
+    public Task Equals_Override()
+    {
+        var test = new AnalyzerTest();
+        test.TestCode = """
+            struct Test
+            {
+                public override bool Equals(object o) => throw null;
+                public override int GetHashCode() => throw null;
+            }
+
+            class Sample
+            {
+                public void A()
+                {
+                    _ = new Test().Equals(new Test());
+                }
+            }
+            """;
+
+        return test.RunAsync();
+    }
+
+    [Fact]
+    public Task GetHashCode_DefaultImplementation()
+    {
+        var test = new AnalyzerTest();
+        test.TestCode = """
+            struct Test
+            {
+            }
+
+            class Sample
+            {
+                public void A()
+                {
+                    _ = {|MA0065:new Test().GetHashCode()|};
+                }
+            }
+            """;
+
+        return test.RunAsync();
+    }
+
+    [Fact]
+    public Task GetHashCode_Override()
+    {
+        var test = new AnalyzerTest();
+        test.TestCode = """
+            struct Test
+            {
+                public override bool Equals(object o) => throw null;
+                public override int GetHashCode() => throw null;
+            }
+
+            class Sample
+            {
+                public void A()
+                {
+                    _ = new Test().GetHashCode();
+                }
+            }
+            """;
+
+        return test.RunAsync();
+    }
+
+    [Fact]
+    public Task GetHashCode_Enum()
+    {
+        var test = new AnalyzerTest();
+        test.TestCode = """
+            enum Test
+            {
+                A,
+                B,
+            }
+
+            class Sample
+            {
+                public void A()
+                {
+                    _ = Test.A.GetHashCode();
+                }
+            }
+            """;
+
+        return test.RunAsync();
+    }
+
+    [Fact]
+    public Task GetHashCode_EnumVariable()
+    {
+        var test = new AnalyzerTest();
+        test.TestCode = """
+            enum Test
+            {
+                A,
+                B,
+            }
+
+            class Sample
+            {
+                public void A()
+                {
+                    var a = Test.A;
+                    _ = a.GetHashCode();
+                }
+            }
+            """;
+
+        return test.RunAsync();
     }
 
     [Theory]
@@ -24,7 +169,7 @@ public sealed class DoNotUseDefaultEqualsOnValueTypeAnalyzer_HashSetTests
     [InlineData("System.Collections.Immutable.ImmutableSortedDictionary<Test, object>.Empty")]
     public Task Constructor_DefaultImplementation(string text)
     {
-        var test = CreateTest();
+        var test = new AnalyzerTest();
         test.TestCode = $$"""
             struct Test
             {
@@ -42,7 +187,7 @@ public sealed class DoNotUseDefaultEqualsOnValueTypeAnalyzer_HashSetTests
     [InlineData("System.Collections.Immutable.ImmutableHashSet<Test>.Empty")]
     public Task Empty_WithComparer(string text)
     {
-        var test = CreateTest();
+        var test = new AnalyzerTest();
         test.TestCode = $$"""
             struct Test
             {
@@ -61,7 +206,7 @@ public sealed class DoNotUseDefaultEqualsOnValueTypeAnalyzer_HashSetTests
     [InlineData("System.Collections.Immutable.ImmutableSortedDictionary<Test, object>.Empty")]
     public Task Empty_WithComparers(string text)
     {
-        var test = CreateTest();
+        var test = new AnalyzerTest();
         test.TestCode = $$"""
             struct Test
             {
@@ -87,7 +232,7 @@ public sealed class DoNotUseDefaultEqualsOnValueTypeAnalyzer_HashSetTests
     [InlineData("System.Collections.Immutable.ImmutableSortedDictionary<Test, object>.Empty")]
     public Task Constructor_EqualsOverriden(string text)
     {
-        var test = CreateTest();
+        var test = new AnalyzerTest();
         test.TestCode = $$"""
             struct Test
             {
@@ -113,7 +258,7 @@ public sealed class DoNotUseDefaultEqualsOnValueTypeAnalyzer_HashSetTests
     [InlineData("System.Collections.Immutable.ImmutableSortedDictionary.Create<Test, object>(null, System.Collections.Generic.EqualityComparer<object>.Default)")]
     public Task Constructor_EqualityComparer(string text)
     {
-        var test = CreateTest();
+        var test = new AnalyzerTest();
         test.TestCode = $$"""
             struct Test
             {
@@ -134,9 +279,9 @@ public sealed class DoNotUseDefaultEqualsOnValueTypeAnalyzer_HashSetTests
     [InlineData("System.Collections.Immutable.ImmutableHashSet.Create<Test>()")]
     [InlineData("System.Collections.Immutable.ImmutableDictionary.Create<Test, object>()")]
     [InlineData("System.Collections.Immutable.ImmutableSortedDictionary.Create<Test, object>()")]
-    public Task GetHashCode_Enum(string text)
+    public Task Constructor_Enum(string text)
     {
-        var test = CreateTest();
+        var test = new AnalyzerTest();
         test.TestCode = $$"""
 
             enum Test
