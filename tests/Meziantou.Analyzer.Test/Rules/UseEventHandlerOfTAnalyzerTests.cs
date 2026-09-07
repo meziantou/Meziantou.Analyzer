bf6c6e690d30ae8a0c922b43f21626d012f61d3f
Gérald Barré <meziantou@users.noreply.github.com>
2026-09-07 11:33:51 -0700

    Do not report MA0046 on events of type EventHandler<TSender, TEventArgs> (#1435)
    * Do not report MA0046 on events of type EventHandler<TSender, TEventArgs>
    
    .NET 10 added the delegate System.EventHandler<in TSender, in TEventArgs>,
    whose only constraints are "allows ref struct": TSender is not constrained to
    object, and TEventArgs is not constrained to System.EventArgs.
    
    MA0046 validated the signature of the delegate invoke method, so an event
    declared as EventHandler<MySender, MyEventArgs> was reported with "The first
    parameter must be of type object". Accept any construction of EventHandler`2 in
    IsValidSignature instead, as the delegate is the type the framework provides to
    declare such events.
    
    * Cover the remaining branches of MA0046 with tests
    
    The rule had no test for the non-generic EventHandler, for the interface
    implementation escape hatch, nor for three of the four messages of
    IsValidSignature. Only the "second parameter" message was reported by a test,
    so the check that rejects a first parameter that is not object, which is the
    one EventHandler<TSender, TEventArgs> was failing, was never exercised.

--- tests/Meziantou.Analyzer.Test/Rules/UseEventHandlerOfTAnalyzerTests.cs
+++ tests/Meziantou.Analyzer.Test/Rules/UseEventHandlerOfTAnalyzerTests.cs
@@ -40,6 +40,52 @@ public sealed class UseEventHandlerOfTAnalyzerTests
     }
 
     [Fact]
+    public Task ValidEvent_NonGenericEventHandler()
+    {
+        var test = CreateTest();
+        test.TestCode = """
+            class Test
+            {
+                event System.EventHandler myevent;
+            }
+            """;
+
+        return test.RunAsync();
+    }
+
+    [Fact]
+    public Task ValidEvent_EventHandlerOfTSenderTEventArgs()
+    {
+        var test = CreateTest();
+        test.TestCode = """
+            class SampleEventArgs : System.EventArgs
+            {
+            }
+
+            class Test
+            {
+                event System.EventHandler<Test, SampleEventArgs> myevent;
+            }
+            """;
+
+        return test.RunAsync();
+    }
+
+    [Fact]
+    public Task ValidEvent_EventHandlerOfTSenderTEventArgs_EventArgsDoesNotInheritFromEventArgs()
+    {
+        var test = CreateTest();
+        test.TestCode = """
+            class Test
+            {
+                event System.EventHandler<Test, string> myevent;
+            }
+            """;
+
+        return test.RunAsync();
+    }
+
+    [Fact]
     public Task ValidEvent_CustomDelegate()
     {
         var test = CreateTest();
@@ -73,8 +119,50 @@ public sealed class UseEventHandlerOfTAnalyzerTests
         return test.RunAsync();
     }
 
+    [Fact]
+    public Task InterfaceImplementation()
+    {
+        var test = CreateTest();
+        test.TestCode = """
+            interface ITest
+            {
+                event System.Action<string> {|MA0046:myevent|};
+            }
+
+            class Test : ITest
+            {
+                public event System.Action<string> myevent;
+            }
+            """;
+
+        return test.RunAsync();
+    }
+
+    [Fact]
+    public Task ExplicitInterfaceImplementation()
+    {
+        var test = CreateTest();
+        test.TestCode = """
+            interface ITest
+            {
+                event System.Action<string> {|MA0046:myevent|};
+            }
+
+            class Test : ITest
+            {
+                event System.Action<string> ITest.myevent { add { } remove { } }
+            }
+            """;
+
+        return test.RunAsync();
+    }
+
     [Theory]
+    [InlineData("System.Action")]
     [InlineData("System.Action<string>")]
+    [InlineData("System.Action<object, System.EventArgs, int>")]
+    [InlineData("System.Action<string, System.EventArgs>")]
+    [InlineData("System.Func<object, System.EventArgs, int>")]
     [InlineData("System.EventHandler<string>")]
     public Task InvalidEvent(string signature)
     {
