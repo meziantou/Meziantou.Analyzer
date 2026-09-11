1971dfbceb9b78a395074c8443fa2f3b01c71a9c
Gérald Barré <meziantou@users.noreply.github.com>
2026-09-11 12:21:09 -0700

    MA0052: Suggest the first declared member when the enum value has aliases (#1455)
    * MA0052: Do not report enum members sharing their value with another member
    
    Enum.ToString formats the value, so when several members have the same
    value it may return the name of another member than the one referenced
    in the source code, while nameof always returns the referenced name.
    Replacing the call with nameof could change the resulting string.
    
    The rule now skips such members, for both ToString calls and
    interpolations.
    
    * MA0052: Suggest the first declared member when the enum value has aliases
    
    Report the diagnostic again for enum members sharing their value with
    other members, but make the code fix use the first declared member, which
    is the name Enum.ToString returns on .NET, instead of the member written
    in the source code. As this behavior is not documented, one alternative
    code fix is provided for each other member sharing the value.

--- tests/Meziantou.Analyzer.Test/Rules/ReplaceEnumToStringWithNameofAnalyzerTests.cs
+++ tests/Meziantou.Analyzer.Test/Rules/ReplaceEnumToStringWithNameofAnalyzerTests.cs
@@ -48,6 +48,172 @@ public sealed class ReplaceEnumToStringWithNameofAnalyzerTests
     }
 
     [Fact]
+    public Task EnumMemberWithAlias_UseFirstDeclaredMember()
+    {
+        var test = CreateTest();
+        test.TestCode = """
+            class Test
+            {
+                void A()
+                {
+                    _ = {|MA0052:MyEnum.B.ToString()|};
+                    _ = {|MA0052:MyEnum.D.ToString("G")|};
+                    _ = $"{|MA0052:{MyEnum.B}|}";
+                }
+            }
+
+            enum MyEnum
+            {
+                A = 0,
+                B = 0,
+                C = 1,
+                D = C,
+            }
+            """;
+        test.FixedCode = """
+            class Test
+            {
+                void A()
+                {
+                    _ = nameof(MyEnum.A);
+                    _ = nameof(MyEnum.C);
+                    _ = $"{nameof(MyEnum.A)}";
+                }
+            }
+
+            enum MyEnum
+            {
+                A = 0,
+                B = 0,
+                C = 1,
+                D = C,
+            }
+            """;
+
+        return test.RunAsync();
+    }
+
+    [Fact]
+    public Task EnumMemberWithAlias_UseOtherMember()
+    {
+        var test = CreateTest();
+        test.CodeActionIndex = 1;
+        test.CodeActionEquivalenceKey = "Use nameof(MyEnum.B)";
+        test.TestCode = """
+            class Test
+            {
+                void A()
+                {
+                    _ = {|MA0052:MyEnum.A.ToString()|};
+                }
+            }
+
+            enum MyEnum
+            {
+                A = 0,
+                B = 0,
+            }
+            """;
+        test.FixedCode = """
+            class Test
+            {
+                void A()
+                {
+                    _ = nameof(MyEnum.B);
+                }
+            }
+
+            enum MyEnum
+            {
+                A = 0,
+                B = 0,
+            }
+            """;
+
+        return test.RunAsync();
+    }
+
+    [Fact]
+    public Task EnumMemberWithAlias_UsingStatic()
+    {
+        var test = CreateTest();
+        test.TestCode = """
+            using static MyEnum;
+
+            class Test
+            {
+                void M()
+                {
+                    _ = {|MA0052:B.ToString()|};
+                }
+            }
+
+            enum MyEnum
+            {
+                A = 0,
+                B = 0,
+            }
+            """;
+        test.FixedCode = """
+            using static MyEnum;
+
+            class Test
+            {
+                void M()
+                {
+                    _ = nameof(A);
+                }
+            }
+
+            enum MyEnum
+            {
+                A = 0,
+                B = 0,
+            }
+            """;
+
+        return test.RunAsync();
+    }
+
+    [Fact]
+    public Task EnumMemberWithAlias_KeywordName()
+    {
+        var test = CreateTest();
+        test.TestCode = """
+            class Test
+            {
+                void A()
+                {
+                    _ = {|MA0052:MyEnum.B.ToString()|};
+                }
+            }
+
+            enum MyEnum
+            {
+                @class = 0,
+                B = 0,
+            }
+            """;
+        test.FixedCode = """
+            class Test
+            {
+                void A()
+                {
+                    _ = nameof(MyEnum.@class);
+                }
+            }
+
+            enum MyEnum
+            {
+                @class = 0,
+                B = 0,
+            }
+            """;
+
+        return test.RunAsync();
+    }
+
+    [Fact]
     public Task EnumVariableToString()
     {
         var test = CreateTest();
