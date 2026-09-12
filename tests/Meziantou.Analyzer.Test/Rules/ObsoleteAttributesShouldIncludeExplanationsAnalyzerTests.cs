db8d01d7c6169d938d26a9af6f7ea9767043b28d
Gérald Barré <meziantou@users.noreply.github.com>
2026-09-12 16:17:49 -0400

    Report MA0070 on every declaration that can be obsolete (#1508)
    MA0070 only registered SymbolKind.Method, so [Obsolete] without an
    explanation was ignored everywhere else: types, properties, indexers,
    fields, events and enum members. Property accessors are methods, but the
    attribute is stored on the IPropertySymbol rather than on get_/set_, so
    the accessor registration did not cover properties either.
    
    Register NamedType, Property, Field and Event as well, and read the
    attributes through ISymbol. Those five kinds cover every target
    ObsoleteAttribute allows (class, struct, enum, interface, delegate,
    constructor, method, property, indexer, field, event), with enum members
    and indexers falling under Field and Property. Parameters and assemblies
    are not valid targets, so they are deliberately left out: the compiler
    already reports CS0592 there, and analyzing them would only add
    diagnostics on code that does not compile.
    
    Skip implicitly declared symbols, which cannot carry an attribute of
    their own, and report an attribute shared by several variable
    declarators only once, since "[Obsolete] int a, b;" produces one
    attribute for two field symbols and reported the same location twice.

--- tests/Meziantou.Analyzer.Test/Rules/ObsoleteAttributesShouldIncludeExplanationsAnalyzerTests.cs
+++ tests/Meziantou.Analyzer.Test/Rules/ObsoleteAttributesShouldIncludeExplanationsAnalyzerTests.cs
@@ -1,4 +1,4 @@
-using AnalyzerTest = Meziantou.Analyzer.Test.Harness.CSharpAnalyzerTest<
+﻿using AnalyzerTest = Meziantou.Analyzer.Test.Harness.CSharpAnalyzerTest<
     Meziantou.Analyzer.Rules.ObsoleteAttributesShouldIncludeExplanationsAnalyzer>;
 
 namespace Meziantou.Analyzer.Test.Rules;
@@ -30,6 +30,294 @@ public sealed class ObsoleteAttributesShouldIncludeExplanationsAnalyzerTests
             class Test
             {
                 [{|MA0070:System.Obsolete()|}]
+                public void A() { }
+            }
+            """;
+
+        return test.RunAsync();
+    }
+
+    [Fact]
+    public Task Class_HasMessage()
+    {
+        var test = CreateTest();
+        test.TestCode = """
+            [System.Obsolete("message")]
+            class Test
+            {
+            }
+            """;
+
+        return test.RunAsync();
+    }
+
+    [Fact]
+    public Task Class_HasNoMessage()
+    {
+        var test = CreateTest();
+        test.TestCode = """
+            [{|MA0070:System.Obsolete|}]
+            class Test
+            {
+            }
+            """;
+
+        return test.RunAsync();
+    }
+
+    [Fact]
+    public Task Interface_HasNoMessage()
+    {
+        var test = CreateTest();
+        test.TestCode = """
+            [{|MA0070:System.Obsolete|}]
+            interface ITest
+            {
+            }
+            """;
+
+        return test.RunAsync();
+    }
+
+    [Fact]
+    public Task Struct_HasNoMessage()
+    {
+        var test = CreateTest();
+        test.TestCode = """
+            [{|MA0070:System.Obsolete|}]
+            struct Test
+            {
+            }
+            """;
+
+        return test.RunAsync();
+    }
+
+    [Fact]
+    public Task Delegate_HasNoMessage()
+    {
+        var test = CreateTest();
+        test.TestCode = """
+            [{|MA0070:System.Obsolete|}]
+            delegate void Test();
+            """;
+
+        return test.RunAsync();
+    }
+
+    [Fact]
+    public Task Enum_HasNoMessage()
+    {
+        var test = CreateTest();
+        test.TestCode = """
+            [{|MA0070:System.Obsolete|}]
+            enum Test
+            {
+            }
+            """;
+
+        return test.RunAsync();
+    }
+
+    [Fact]
+    public Task EnumMember_HasNoMessage()
+    {
+        var test = CreateTest();
+        test.TestCode = """
+            enum Test
+            {
+                [{|MA0070:System.Obsolete|}]
+                A,
+            }
+            """;
+
+        return test.RunAsync();
+    }
+
+    [Fact]
+    public Task Constructor_HasNoMessage()
+    {
+        var test = CreateTest();
+        test.TestCode = """
+            class Test
+            {
+                [{|MA0070:System.Obsolete|}]
+                public Test() { }
+            }
+            """;
+
+        return test.RunAsync();
+    }
+
+    [Fact]
+    public Task Property_HasMessage()
+    {
+        var test = CreateTest();
+        test.TestCode = """
+            class Test
+            {
+                [System.Obsolete("message")]
+                public int A { get; set; }
+            }
+            """;
+
+        return test.RunAsync();
+    }
+
+    [Fact]
+    public Task Property_HasNoMessage()
+    {
+        var test = CreateTest();
+        test.TestCode = """
+            class Test
+            {
+                [{|MA0070:System.Obsolete|}]
+                public int A { get; set; }
+            }
+            """;
+
+        return test.RunAsync();
+    }
+
+    [Fact]
+    public Task PropertyAccessor_HasNoMessage()
+    {
+        var test = CreateTest();
+        test.TestCode = """
+            class Test
+            {
+                public int A
+                {
+                    [{|MA0070:System.Obsolete|}]
+                    get => 0;
+                }
+            }
+            """;
+
+        return test.RunAsync();
+    }
+
+    [Fact]
+    public Task Indexer_HasNoMessage()
+    {
+        var test = CreateTest();
+        test.TestCode = """
+            class Test
+            {
+                [{|MA0070:System.Obsolete|}]
+                public int this[int index] => 0;
+            }
+            """;
+
+        return test.RunAsync();
+    }
+
+    [Fact]
+    public Task Field_HasMessage()
+    {
+        var test = CreateTest();
+        test.TestCode = """
+            class Test
+            {
+                [System.Obsolete("message")]
+                public int A;
+            }
+            """;
+
+        return test.RunAsync();
+    }
+
+    [Fact]
+    public Task Field_HasNoMessage()
+    {
+        var test = CreateTest();
+        test.TestCode = """
+            class Test
+            {
+                [{|MA0070:System.Obsolete|}]
+                public int A;
+            }
+            """;
+
+        return test.RunAsync();
+    }
+
+    [Fact]
+    public Task Field_MultipleDeclarators_HasNoMessage_ReportsOnce()
+    {
+        var test = CreateTest();
+        test.TestCode = """
+            class Test
+            {
+                [{|MA0070:System.Obsolete|}]
+                public int A, B;
+            }
+            """;
+
+        return test.RunAsync();
+    }
+
+    [Fact]
+    public Task Event_HasNoMessage()
+    {
+        var test = CreateTest();
+        test.TestCode = """
+            class Test
+            {
+                [{|MA0070:System.Obsolete|}]
+                public event System.EventHandler A;
+            }
+            """;
+
+        return test.RunAsync();
+    }
+
+    [Fact]
+    public Task Event_MultipleDeclarators_HasNoMessage_ReportsOnce()
+    {
+        var test = CreateTest();
+        test.TestCode = """
+            class Test
+            {
+                [{|MA0070:System.Obsolete|}]
+                public event System.EventHandler A, B;
+            }
+            """;
+
+        return test.RunAsync();
+    }
+
+    [Fact]
+    public Task Record_HasNoMessage()
+    {
+        var test = CreateTest();
+        test.TestCode = """
+            [{|MA0070:System.Obsolete|}]
+            record Test(int A);
+            """;
+
+        return test.RunAsync();
+    }
+
+    [Fact]
+    public Task RecordProperty_HasNoMessage()
+    {
+        var test = CreateTest();
+        test.TestCode = """
+            record Test([property: {|MA0070:System.Obsolete|}] int A);
+            """;
+
+        return test.RunAsync();
+    }
+
+    [Fact]
+    public Task HasOnlyNamedArgument()
+    {
+        var test = CreateTest();
+        test.TestCode = """
+            class Test
+            {
+                [{|MA0070:System.Obsolete(DiagnosticId = "ID1")|}]
                 public void A() { }
             }
             """;
