b3379eecc37d5b4cc178f7903bb8774adc89dd27
Gérald Barré <meziantou@users.noreply.github.com>
2026-09-22 23:35:43 -0400

    Fix MA0103 code fix when the left operand is implicitly converted to a span (#1566)

--- tests/Meziantou.Analyzer.Test/Rules/DoNotUseEqualityOperatorsForSpanOfCharAnalyzerTests.cs
+++ tests/Meziantou.Analyzer.Test/Rules/DoNotUseEqualityOperatorsForSpanOfCharAnalyzerTests.cs
@@ -1,3 +1,4 @@
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Testing;
 using CodeFixTest = Meziantou.Analyzer.Test.Harness.CSharpCodeFixTest<
     Meziantou.Analyzer.Rules.DoNotUseEqualityOperatorsForSpanOfCharAnalyzer,
@@ -76,6 +77,93 @@ public sealed class DoNotUseEqualityOperatorsForSpanOfCharAnalyzerTests
                 void A()
                 {
                     _ = "a" == "ab";
+                }
+            }
+            """;
+
+        return test.RunAsync();
+    }
+
+    [Fact]
+    public Task StringEqualsSpan_CSharp12()
+    {
+        var test = CreateTest();
+        test.LanguageVersion = LanguageVersion.CSharp12;
+        test.TestCode = """
+            using System;
+            class Test
+            {
+                void A(ReadOnlySpan<char> s)
+                {
+                    _ = {|MA0103:"a" == s|};
+                }
+            }
+            """;
+        test.FixedCode = """
+            using System;
+            class Test
+            {
+                void A(ReadOnlySpan<char> s)
+                {
+                    _ = "a".AsSpan().SequenceEqual(s);
+                }
+            }
+            """;
+
+        return test.RunAsync();
+    }
+
+    [Fact]
+    public Task SpanNotEqualsString_CSharp12()
+    {
+        var test = CreateTest();
+        test.LanguageVersion = LanguageVersion.CSharp12;
+        test.TestCode = """
+            using System;
+            class Test
+            {
+                void A(ReadOnlySpan<char> s)
+                {
+                    _ = {|MA0103:s != "a"|};
+                }
+            }
+            """;
+        test.FixedCode = """
+            using System;
+            class Test
+            {
+                void A(ReadOnlySpan<char> s)
+                {
+                    _ = !s.SequenceEqual("a");
+                }
+            }
+            """;
+
+        return test.RunAsync();
+    }
+
+    [Fact]
+    public Task CharArrayEqualsSpan_CSharp12()
+    {
+        var test = CreateTest();
+        test.LanguageVersion = LanguageVersion.CSharp12;
+        test.TestCode = """
+            using System;
+            class Test
+            {
+                void A(char[] a, ReadOnlySpan<char> s)
+                {
+                    _ = {|MA0103:a == s|};
+                }
+            }
+            """;
+        test.FixedCode = """
+            using System;
+            class Test
+            {
+                void A(char[] a, ReadOnlySpan<char> s)
+                {
+                    _ = ((ReadOnlySpan<char>)a).SequenceEqual(s);
                 }
             }
             """;
